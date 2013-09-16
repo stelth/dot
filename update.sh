464905d0e9962c87c3ca5f170cfb87aecf6a851d
jcox <jcox@1e9be138-1c13-b74e-86d5-6c63b1325fad>
2013-09-16 15:14:13 +0000

    Modify update script to automatically pull git svn updates
    Former-commit-id: 4349c401b2e61aae58b46feaf5258bd888e7e14e

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+git svn rebase
+vim +NeoBundleInstall! +qall
 cd zsh/.oh-my-zsh
 git pull --rebase
 cd custom/plugins/zsh-syntax-highlighting
 git pull --rebase
-vim +NeoBundleInstall! +qall
