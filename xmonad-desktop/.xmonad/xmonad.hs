2d41ca6d1b3c1336c7b424fe3c0d24cf76f7dbb0
jcox <jcox@1e9be138-1c13-b74e-86d5-6c63b1325fad>
2014-02-18 15:52:37 +0000

    Remove dmenu-with-yeganesh script, run yeganesh directly
    Former-commit-id: 6af026dffc6429935379f2a28124c3c1d7bd7b52

--- xmonad-desktop/.xmonad/xmonad.hs
+++ xmonad-desktop/.xmonad/xmonad.hs
@@ -141,7 +141,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- Launch dmenu via yeganesh.
   -- Use this to launch programs without a key binding.
   , ((modMask, xK_p),
-     spawn "dmenu-with-yeganesh")
+     spawn "yeganesh -x")
 
   -- Take a screenshot in select mode.
   -- After pressing this key binding, click a window, or draw a rectangle with
