4e425af9c8493f3b6faa6c77b009bae325660dde
jcox <jcox@1e9be138-1c13-b74e-86d5-6c63b1325fad>
2014-03-03 14:57:10 +0000

    Update install.sh to not try and clone stuff that exists already
    Former-commit-id: fd37c3f22035440b4747c2e878ecb8bb73f6664d

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-git clone git://github.com/Shougo/neobundle.vim vim/.vim/bundle/neobundle.vim
-git clone git://github.com/robbyrussell/oh-my-zsh.git zsh/.oh-my-zsh
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone git://github.com/chriskempson/base16-shell zsh/base16-shell
+NEOBUNDLE_PATH="vim/.vim/bundle/neobundle.vim"
+OHMYZSH_PATH="zsh/.oh-my-zsh"
+SYNTAX_HIGHLIGHTING_PATH="zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+BASE16_PATH="zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+AUTOSUGGEST_PATH="zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+
+if [ ! -d "$NEOBUNDLE_PATH" ]; then
+	git clone git://github.com/Shougo/neobundle.vim vim/.vim/bundle/neobundle.vim
+fi
+
+if [ ! -d "$OHMYZSH_PATH" ]; then
+	git clone git://github.com/robbyrussell/oh-my-zsh.git zsh/.oh-my-zsh
+fi
+
+if [ ! -d "$SYNTAX_HIGHLIGHTING_PATH" ]; then
+	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$BASE16_PATH" ]; then
+	git clone git://github.com/chriskempson/base16-shell zsh/base16-shell
+fi
+
+if [ ! -d "$AUTOSUGGEST_PATH" ]; then
+	git clone git://github.com/tarruda/zsh-autosuggestions zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
