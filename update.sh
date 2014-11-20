38e0857996f33b8e0ebdf9be08ec770a26c5b56d
Jason Cox <jasonc@teamplayinc.com>
2014-11-20 13:43:08 -0600

    Having update script update prezto instead of oh-my-zsh

--- update.sh
+++ update.sh
@@ -3,11 +3,5 @@
 git pull --rebase
 vim +NeoBundleInstall! +qall
 vim +NeoBundleClean! +qall
-cd ~/.dotfiles/zsh/.oh-my-zsh
-git pull --rebase
-cd ~/.dotfiles/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git pull --rebase
-cd ~/.dotfiles/zsh/base16-shell
-git pull --rebase
-cd ~/.dotfiles/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git pull --rebase
+cd ~/.dotfiles/zsh/.zprezto
+git pull && git submodule update --init --recursive
