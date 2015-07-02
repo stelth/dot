a605941493f925c58686871cdd111e7b399a1002
Jason Cox <jasonc@teamplayinc.com>
2015-07-02 16:31:17 -0500

    Add emacs config

---
+++ emacs/.emacs.d/lisp/init-color-theme.el
@@ -0,0 +1,5 @@
+(require 'color-theme)
+
+(load-theme 'zenburn t)
+
+(provide 'init-color-theme)
---
+++ emacs/.emacs.d/lisp/init-irony.el
@@ -0,0 +1,19 @@
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(defun my-irony-mode-hook  ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'iron-cdb-autosetup-compile-options)
+
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
+(provide 'init-irony)
