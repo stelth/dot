2c0bb952605c8346a4f4607fb90c935e9dacce6f
Jason Cox <jasonc@teamplayinc.com>
2016-04-01 08:54:57 -0500

    Hide update output

--- update.sh
+++ update.sh
@@ -5,25 +5,25 @@ source functions.sh
 
 update_local() {
 	ebegin "Updating local config"
-	git pull --rebase
+	git pull --rebase > /dev/null 2>&1
 	eend $?
 }
 update_local
 
 update_vimplug() {
 	ebegin "Updating vim plugins"
-	vim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'PlugClean!' -c 'qall'
+	vim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'PlugClean!' -c 'qall' > /dev/null 2>&1
 	eend $?
 }
 update_vimplug
 
 update_zsh() {
 	ebegin "Updating zgen"
-	zgen selfupdate
+	zgen selfupdate > /dev/null 2>&1
 	eend $?
 
 	ebegin "Updating zgen plugins"
-	zgen update
+	zgen update > /dev/null 2>&1
 	eend $?
 }
 update_zsh
