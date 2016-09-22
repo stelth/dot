1fff63dcd78c55917c43f34dba680d3f1b78de96
Jason Cox <Jason.P.Cox@ibm.com>
2016-09-22 15:14:51 -0500

    Change update.sh to not need functions.sh
    remove functions.sh

--- update.sh
+++ update.sh
@@ -1,29 +1,24 @@
 #!/bin/zsh
 
-source ~/.dotfiles/antigen/antigen.zsh
-source functions.sh
+source ~/dotfiles/antigen/antigen.zsh
 
 update_local() {
-	ebegin "Updating local config"
+	echo "Updating local config"
 	git pull --rebase > /dev/null 2>&1
-	eend $?
 }
 update_local
 
 update_vimplug() {
-	ebegin "Updating vim plugins"
+	echo "Updating vim plugins"
 	vim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'PlugClean!' -c 'qall' > /dev/null 2>&1
-	eend $?
 }
 update_vimplug
 
 update_zsh() {
-	ebegin "Updating antigen"
+	echo "Updating antigen"
 	antigen selfupdate > /dev/null 2>&1
-	eend $?
 
-	ebegin "Updating antigen plugins"
+	echo "Updating antigen plugins"
 	antigen update > /dev/null 2>&1
-	eend $?
 }
 update_zsh
