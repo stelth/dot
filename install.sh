bbac274924edfd77055885026048fae2aeccf91d
Jason Cox <Jason.P.Cox@ibm.com>
2016-09-22 15:08:27 -0500

    Remove reference to functions.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,38 @@
 #!/bin/zsh
 
-source functions.sh
-
 check_environment() {
-	ebegin "Checking environment"
+	echo "Checking environment"
 
 	required_exes=(git make stow)
 
 	for e in ${required_exes[@]}; do
 		hash $e || {
 			echo "!! Missing: $e"
-			eend 1
 			exit 1
 		}
 	done
-	eend 0
 }
 check_environment
 
 install_antigen() {
-	ebegin "Installing antigeh"
+	echo "Installing antigeh"
 	git clone https://github.com/zsh-users/antigen.git
-	eend $?
 }
 install_antigen
 
 symlink_dotfiles() {
 	for dst in `cat packages`; do
 		nm=${dst##*/}
-		ebegin "Symlinking module: ${nm}"
+		echo "Symlinking module: ${nm}"
 		stow $nm > /dev/null 2>&1
-		eend $?
 	done
 }
 symlink_dotfiles
 
 install_vim() {
 	if [ -L ~/.vim ]; then
-		ebegin "Installing Vim Plugins"
+		echo "Installing Vim Plugins"
 		vim -u vim/.vim/plugins.vim +PlugInstall +qall > /dev/null 2>&1
-		eend $?
 	fi
 }
 install_vim
