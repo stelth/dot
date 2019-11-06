3247c8ab702feb17cdb7fb9fcecef47acc75fbbb
Jason Cox <Jason.P.Cox@ibm.com>
2019-11-06 07:07:14 -0600

    Only update npm if we're root

--- update.sh
+++ update.sh
@@ -68,7 +68,9 @@ update_pip3_packages || true
 
 update_npm_packages() {
 	(( $+commands[npm] )) && {
-		npm install -g npm
+		if [[ "`echo $UID`" == "0" ]] && {
+			npm install -g npm
+		}
 		echo "Updating npm packages"
 		npm --depth 1000 update --dev
 		npm audit fix
