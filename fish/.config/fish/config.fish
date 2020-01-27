ed1e8315a76c4e718c7f937614630276b34a3ca6
Jason Cox <Jason.P.Cox@ibm.com>
2020-01-27 08:31:46 -0600

    Add brew path to path when on mac

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,10 @@ function cd
   builtin cd $argv; and ls
 end
 
+if type -q brew
+    set -U fish_user_paths /usr/local/sbin $fish_user_paths
+end
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
