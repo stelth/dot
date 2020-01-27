637b934249fd6d55a2764b5442ed5652770cfa67
Jason Cox <Jason.P.Cox@ibm.com>
2020-01-27 13:11:53 -0600

    Revert "Fix update script for fish"
    This reverts commit e224bcb199f22a8a5c8f6358f80f453af0e32a9a.

--- update.sh
+++ update.sh
@@ -1,48 +1,48 @@
-#!/usr/bin/env fish
+#!/bin/zsh
 
-function update_local
-    if type -q git
-        echo Updating local config
-        git pull 2>&1 | grep -E "up-to-date|up to date" || { ./update.sh; exit $status}
-        git push
-        git submodule init
-        git submodule update --remote --merge
-    end
-end
-update_local
+update_local() {
+	(( $+commands[git] )) && {
+		echo "Updating local config"
+		git pull 2>&1 | grep -E "up-to-date|up to date" || { ./update.sh; exit $?}
+		git push
+                git submodule init
+                git submodule update --remote --merge
+	}
+}
+update_local || true
 
-function update_brew
-    if type -q brew
-        echo "Updating Home Brew packages"
-        brew update
-        brew upgrade
-        brew cleanup
-        brew cleanup -s
-        brew doctor
-        brew missing
-    end
-end
-update_brew
+update_brew() {
+	(( $+commands[brew] )) && {
+		echo "Updating Home Brew packages"
+		brew update
+		brew upgrade
+		brew cleanup
+		brew cleanup -s
+		brew doctor
+		brew missing
+	}
+}
+update_brew || true
 
-function update_apt
-    if test (id -u) -eq 0
-        if type -q apt
-            if type -q apt-get
-                echo "Updating apt packages"
-                apt-get update -y --allow-unauthenticated
-                apt-get upgrade -y -f --allow-unauthenticated
-                apt-get dist-upgrade -y -f --allow-unauthenticated
-                apt autoremove -y -f
-            end
-        end
-    end
-end
-update_apt
+update_apt() {
+	[[ "`echo $UID`" == "0" ]] && {
+		(( $+commands[apt] )) && {
+			(( $+commands[apt-get] )) && {
+				echo "Updating apt packages"
+				apt-get update -y --allow-unauthenticated
+				apt-get upgrade -y -f --allow-unauthenticated
+				apt-get dist-upgrade -y -f --allow-unauthenticated
+				apt autoremove -y -f
+			}
+		}
+	}
+}
+update_apt || true
 
-function update_pip3_packages
-    if type -q pip3
-        echo "Updating pip3 packages"
-        pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U --user
-    end
-end
-update_pip3_packages
+update_pip3_packages() {
+	(( $+commands[pip3] )) && {
+		echo "Updating pip3 packages"
+		pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U --user
+	}
+}
+update_pip3_packages || true
