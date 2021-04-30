637d1c1b3b1d3a07ac60c5386cbda501898695d2
Jason Cox <Jason.P.Cox@ibm.com>
2021-04-30 12:24:48 -0500

    Update update_utils syntax to be more zsh-y and add update_npm_packages

--- update_utils.zsh
+++ update_utils.zsh
@@ -12,7 +12,7 @@ update_brew() {
 }
 
 update_apt() {
-    [[ "`echo $UID`" == "0" ]] && {
+    if [[ "`echo $UID`" == "0" ]]; then
         if (( $+commands[apt] )); then
             if (( $+commands[apt-get] )); then
                 apt-get update -y --allow-unauthenticated
@@ -21,15 +21,15 @@ update_apt() {
                 apt autoremove -y -f
             fi
         fi
-    }
+    fi
 }
 
 update_nvim() {
-    if [ `uname` = Linux ]; then
+    if [[ `uname` = Linux ]]; then
         wget "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage" -O bin/nvim
         chmod u+x bin/nvim
     fi
-    if [ `uname` = Darwin ]; then
+    if [[ `uname` = Darwin ]]; then
         wget "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
         tar xvf nvim-macos.tar.gz -C bin nvim-osx64/bin/nvim
         mv bin/nvim-osx64/bin/nvim bin/nvim
@@ -38,13 +38,13 @@ update_nvim() {
 }
 
 update_ghq() {
-    if [ `uname` = Linux ]; then
+    if [[ `uname` = Linux ]]; then
         wget https://github.com/x-motemen/ghq/releases/latest/download/ghq_linux_amd64.zip
         unzip -o -j "ghq_linux_amd64.zip" "ghq_linux_amd64/ghq" -d "bin"
         rm ghq_linux_amd64.zip
     fi
 
-    if [ `uname` = Darwin ]; then
+    if [[ `uname` = Darwin ]]; then
         wget https://github.com/x-motemen/ghq/releases/latest/download/ghq_darwin_amd64.zip
         unzip -o -j "ghq_darwin_amd64.zip" "ghq_darwin_amd64/ghq" -d "bin"
         rm ghq_darwin_amd64.zip
@@ -56,4 +56,13 @@ update_pip3_packages() {
        echo "Updating pip3 packages"
        pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U --user
    fi
+}
+
+update_npm_packages() {
+    if [[ ( `uname` = Linux && `whoami` = root ) || `uname` = Darwin ]]; then
+        if (( $+commands[npm] )); then
+            cat npm-packages.txt | xargs npm install -g
+            npm update -g
+        fi
+    fi
 }
