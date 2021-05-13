8127f6364c8e6d8a5f7591508fdde471b7f08541
Jason Cox <Jason.P.Cox@ibm.com>
2021-05-13 10:07:24 -0500

    Remove second vista mapping

--- nvim/nvim/lua/modules/tools/config.lua
+++ nvim/nvim/lua/modules/tools/config.lua
@@ -5,14 +5,6 @@ function config.vim_vista()
     vim.g.vista_disable_statusline = 1
     vim.g.vista_default_executive = 'nvim_lsp'
     vim.g.vista_echo_cursor_strategy = 'floating_win'
-
-    local wk = require("which-key")
-
-    wk.register({
-        ["<leader>"] = {
-            v = { "<cmd>Vista<CR>", "Vista" }
-        }
-    } )
 end
 
 function config.fterm()
