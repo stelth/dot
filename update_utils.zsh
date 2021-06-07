9e3bb0ff30ae6f7817b0466e97861107b46c2ec4
Jason Cox <Jason.P.Cox@ibm.com>
2021-06-07 14:52:22 -0500

    Move nvim binaries to nvim_bin

--- update_utils.zsh
+++ update_utils.zsh
@@ -29,13 +29,18 @@ update_apt() {
 }
 
 update_nvim() {
-    if [[ `uname` = Linux ]]; then
-        wget "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage" -O bin/nvim.appimage
-        chmod u+x bin/nvim.appimage
+    if [[ ! -d 'nvim_bin' ]]; then
+        mkdir nvim_bin
     fi
+
+    if [[ `uname` = Linux ]]; then
+        wget "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage" -O nvim_bin/nvim.appimage
+        chmod u+x nvim_bin/nvim.appimage
+    fi
+
     if [[ `uname` = Darwin ]]; then
         wget "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
-        tar xvf nvim-macos.tar.gz -C bin
+        tar xvf nvim-macos.tar.gz -C nvim_bin
         rm -rf nvim-macos.tar.gz
     fi
 }
