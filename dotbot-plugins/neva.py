b9cade674cec937b1c2fa92feab26af8d2c0873e
Jason Cox <Jason.P.Cox@ibm.com>
2021-06-12 12:39:02 -0500

    Use neva neovim package manager, write dotbot plugin for it

--- dotbot-plugins/neva.py
+++ dotbot-plugins/neva.py
@@ -3,11 +3,15 @@ import dotbot
 import imp
 import pathlib
 path = "%s/package.py" % pathlib.Path(__file__).parent.absolute()
+installer = '%s/neva' % pathlib.Path(__file__).parent.absolute()
 
 package = imp.load_source('package', path)
 
-@package.updateable
+
+@package.installable
 class Neovim(package.PackageHandler, dotbot.Plugin):
     def __init__(self, context):
-        self._directive = 'neovim'
+        self._directive = 'neva'
+        self._install_cmds = ['%s uninstall' % installer,
+                              '%s install' % installer, '%s clean' % installer]
         super(Neovim, self).__init__(context)
