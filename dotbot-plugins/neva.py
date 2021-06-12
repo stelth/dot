8331e850b9374ca73ed2b5225d1b5cd2bf94e681
Jason Cox <Jason.P.Cox@ibm.com>
2021-06-12 12:47:53 -0500

    Cleanup path to neva installer

--- dotbot-plugins/neva.py
+++ dotbot-plugins/neva.py
@@ -3,7 +3,6 @@ import dotbot
 import imp
 import pathlib
 path = "%s/package.py" % pathlib.Path(__file__).parent.absolute()
-installer = '%s/neva' % pathlib.Path(__file__).parent.absolute()
 
 package = imp.load_source('package', path)
 
@@ -11,6 +10,7 @@ package = imp.load_source('package', path)
 @package.installable
 class Neovim(package.PackageHandler, dotbot.Plugin):
     def __init__(self, context):
+        installer = '%s/neva' % pathlib.Path(__file__).parent.absolute()
         self._directive = 'neva'
         self._install_cmds = ['%s uninstall' % installer,
                               '%s install' % installer, '%s clean' % installer]
