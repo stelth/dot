bb16740df8a5eae2df965867612386d1ae94c707
Jason Cox <Jason.P.Cox@ibm.com>
2021-06-15 10:02:25 -0500

    Add debugger support for c and c++

--- config/nvim/lua/modules/debugger/config.lua
+++ config/nvim/lua/modules/debugger/config.lua
@@ -3,6 +3,29 @@ local config = {}
 function config.nvim_dap()
     require('dap-python').setup('python3')
 
+    local dap = require('dap')
+    dap.adapters.lldb = {
+        type = 'executable',
+        command = 'lldb-vscode',
+        name = 'lldb'
+    }
+
+    dap.configurations.cpp = {
+        {
+            name = 'Launch',
+            type = 'lldb',
+            request = "launch",
+            program = function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = {}
+        }
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+
     require('modules.debugger.dapkeymaps').setup_keymaps()
 end
 
