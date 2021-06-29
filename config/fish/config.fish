b1ca7d37de5b768d4c5afe8de6f75fdb6fc7524c
Jason Cox <Jason.P.Cox@ibm.com>
2021-06-29 14:16:34 -0500

    feat(fish): use the right stat command depending on OS

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,12 @@
-set -l fish_config_mtime (/usr/bin/stat -c %Y $__fish_config_dir/config.fish)
+switch (uname)
+    case Linux
+        set -l fish_config_mtime (/usr/bin/stat -c %Y $__fish_config_dir/config.fish)
+    case Darwin
+        set -l fish_config_mtime (/usr/bin/stat -f %m $__fish_config_dir/config.fish)
+end
 
 set -gx EDITOR nvim
+
 if test "$fish_config_changed" = "$fish_config_mtime"
     exit
 else
