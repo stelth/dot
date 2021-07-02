77647392c3886a552f5804c189b752b0bdc82bb9
Jason Cox <Jason.P.Cox@ibm.com>
2021-07-02 09:54:34 -0500

    fix(fish): stat on macOS and linux behave differently

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,15 @@
+if test "(uname)" = Darwin
+    set -l fish_config_mtime (/usr/bin/stat -f %m $__fish_config_dir/config.fish)
+else if test "(uname)" = Linux
+    set -l fish_config_mtime (/usr/bin/stat -c %Y $__fish_config_dir/config.fish)
+end
+
 set -gx EDITOR nvim
+if test "$fish_config_changed" = "$fish_config_mtime"
+    exit
+else
+    set -U fish_config_changed $fish_config_mtime
+end
 
 set -Ux fish_user_paths
 
