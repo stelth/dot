639e0197fa120cf7f15b81c473206c3440b97ab9
Jason Cox <Jason.P.Cox@ibm.com>
2021-07-19 13:36:22 -0500

    feat(nix): Move more ssh config to declarative format

--- nix/modules/util/ssh.nix
+++ nix/modules/util/ssh.nix
@@ -1,14 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   programs.ssh = {
     enable = true;
-    userKnownHostsFile = "/dev/null";
-    serverAliveInterval = 50;
 
     matchBlocks = {
       "10.* 172.18.30.* 172.19.* 192.168.*" = {
         user = "root";
         identityFile = "~/dot/config/ssh/id_rsa.cleversafelabs";
+      };
+      "github.com" = lib.hm.dag.entryBefore [ "10.* 172.18.30.* 172.19.* 192.168.*" ] {
+        user = "stelth";
+        identityFile = "~/dot/config/ssh/id_ed25519_github";
       };
     };
   };
