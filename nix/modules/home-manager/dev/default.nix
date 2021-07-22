a10db86bdae79e36228b8ebe0fa22f5333172131
Jason Cox <steel300@gmail.com>
2021-07-22 11:37:32 -0500

    chore(nix): Update path to ssh identity files

--- nix/modules/home-manager/dev/default.nix
+++ nix/modules/home-manager/dev/default.nix
@@ -23,16 +23,16 @@
     matchBlocks = {
       "10.* 172.18.30.* 172.19.* 192.168.*" = {
         user = "root";
-        identityFile = "~/dot/config/ssh/id_rsa.cleversafelabs";
+        identityFile = "~/dot/nix/modules/home-manager/dotfiles/ssh/id_rsa.cleversafelabs";
         serverAliveInterval = 50;
       };
       "github.com" = lib.hm.dag.entryBefore [ "10.* 172.18.30.* 172.19.* 192.168.*" ] {
         user = "stelth";
-        identityFile = "~/dot/config/ssh/id_ed25519_github";
+        identityFile = "~/dot/nix/modules/home-manager/dotfiles/ssh/id_ed25519_github";
       };
       "github.ibm.com" = lib.hm.dag.entryBefore [ "github.com" ] {
+        identityFile = "~/dot/nix/modules/home-manager/dotfiles/ssh/id_ed25519_ibm";
         user = "Jason.P.Cox@ibm.com";
-        identityFile = "~/dot/config/ssh/id_ed25519_ibm";
       };
     };
   };
