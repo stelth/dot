57cc55ec9480470a9fc91db893a143a611380f1f
Jason Cox <steel300@gmail.com>
2021-08-06 11:22:06 -0500

    feat(nix): Add nix install command and sysdo python script for easier management of nix builds

---
+++ bin/install-nix.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+RELEASE="nix-2.4pre20210604_8e6ee1b"
+URL="https://github.com/numtide/nix-unstable-installer/releases/download/$RELEASE/install"
+
+# install using workaround for darwin systems
+[[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
+[[ -n "$1" ]] && URL="$1"
+
+if command -v nix >/dev/null; then
+	echo "nix is already installed on this system."
+else
+	bash <(curl -L "$URL") --daemon "$FLAG"
+fi
