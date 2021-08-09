f3bcbfe776c8a96946c5a6de5a761e0246d29602
Jason Cox <steel300@gmail.com>
2021-08-09 10:21:11 -0500

    fix(nix): Fix homeDirectory directory

--- flake.nix
+++ flake.nix
@@ -132,7 +132,7 @@
           }:
             homeManagerConfiguration rec {
               inherit system username;
-              homeDirectory = "/${homePrefix system}/${username}";
+              homeDirectory = "${homePrefix system}/${username}";
               extraSpecialArgs = { inherit inputs lib; };
               configuration = {
                 imports = baseModules ++ extraModules
