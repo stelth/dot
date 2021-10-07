cae34f25beecc6e29d472dcd94a138958940e58c
Jason Cox <steel300@gmail.com>
2021-10-07 11:59:49 -0500

    chore(neovim): Simplify some of the dap key mappings

--- modules/home-manager/nvim/nvim/lua/config/dap/keys.lua
+++ modules/home-manager/nvim/nvim/lua/config/dap/keys.lua
@@ -14,16 +14,20 @@ M.setup = function(_, bufnr)
           "Set Conditional Breakpoint",
         },
         c = { "<cmd>lua require('dap').continue()<CR>", "Continue" },
-        dr = { "<cmd>lua require('dap').repl.open()<CR>", "REPL" },
-        dl = { "<cmd>lua require('dap').run_last()<CR>", "Run Last" },
-        lp = {
+        r = { "<cmd>lua require('dap').repl.open()<CR>", "REPL" },
+        l = { "<cmd>lua require('dap').run_last()<CR>", "Run Last" },
+        m = {
           "<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
           "Log point",
         },
-        si = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
-        st = { "<cmd>lua require('dap').step_out()<CR>", "Step Out" },
-        so = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
-        ui = { "<cmd>lua require('dapui').toggle()<CR>", "UI" },
+        i = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
+        t = { "<cmd>lua require('dap').step_out()<CR>", "Step Out" },
+        o = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
+        u = {
+          name = "+ui",
+          t = { "<cmd>lua require('dapui').toggle()<CR>", "Toggle" },
+          f = { "<cmd>lua require('dapui').float_element()<CR>", "Float" },
+        },
       },
     },
   }
