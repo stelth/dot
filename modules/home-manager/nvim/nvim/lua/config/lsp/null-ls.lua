e9270366088900407047a08c4269bcafe28547b7
Jason Cox <steel300@gmail.com>
2021-10-26 09:04:00 -0500

    fix(neovim): Disable proselint until they solve their cache problems

--- modules/home-manager/nvim/nvim/lua/config/lsp/null-ls.lua
+++ modules/home-manager/nvim/nvim/lua/config/lsp/null-ls.lua
@@ -32,7 +32,7 @@ function M.setup()
       nls.builtins.diagnostics.hadolint,
       nls.builtins.diagnostics.luacheck,
       nls.builtins.diagnostics.markdownlint,
-      nls.builtins.diagnostics.proselint,
+      -- nls.builtins.diagnostics.proselint,
       nls.builtins.diagnostics.pylint,
       nls.builtins.diagnostics.selene,
       nls.builtins.diagnostics.shellcheck,
@@ -40,7 +40,7 @@ function M.setup()
       nls.builtins.diagnostics.write_good,
 
       nls.builtins.code_actions.gitsigns,
-      nls.builtins.code_actions.proselint,
+      -- nls.builtins.code_actions.proselint,
 
       nls.builtins.hover.dictionary,
     },
