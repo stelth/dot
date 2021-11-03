d4ed878663b8e39f167048d863d0d7609c8e28b8
Jason Cox <steel300@gmail.com>
2021-11-02 22:21:51 -0500

    chore(neovim): Remove the doc generation, it's never used

--- modules/home-manager/nvim/nvim/lua/util/init.lua
+++ modules/home-manager/nvim/nvim/lua/util/init.lua
@@ -155,18 +155,6 @@ function M.float_terminal(cmd)
   vim.cmd([[startinsert]])
 end
 
-function M.docs()
-  local name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-  local docgen = require("babelfish")
-  vim.fn.mkdir("./doc", "p")
-  local metadata = {
-    input_file = "./README.md",
-    output_file = "doc/" .. name .. ".txt",
-    project_name = name,
-  }
-  docgen.generate_readme(metadata)
-end
-
 function M.lsp_config()
   local ret = {}
   for _, client in pairs(vim.lsp.get_active_clients()) do
