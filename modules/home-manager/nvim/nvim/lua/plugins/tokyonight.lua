9802d8ede7a829e54427ae3dc0394fc0842f99c9
Jason Cox <steel300@gmail.com>
2021-11-09 20:41:58 -0600

    chore(neovim): Remove config options that don't exist

--- modules/home-manager/nvim/nvim/lua/plugins/tokyonight.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/tokyonight.lua
@@ -1,28 +1,6 @@
 local M = {}
 
 local setup = function()
-  vim.g.material_style = "pale_night"
-  vim.g.material_italic_comments = 1
-  vim.g.material_italic_keywords = 1
-  vim.g.material_italic_functions = 1
-  vim.g.material_lsp_underline = 1
-
-  vim.g.sonokai_style = "atlantis"
-  vim.g.sonokai_enable_italic = 1
-  vim.g.sonokai_disable_italic_comment = 1
-  vim.g.sonokai_diagnostic_virtual_text = "colored"
-
-  vim.g.edge_style = "neon"
-  vim.g.edge_enable_italic = 1
-  vim.g.edge_disable_italic_comment = 0
-  vim.g.edge_transparent_background = 0
-
-  vim.g.embark_terminal_italics = 1
-
-  vim.g.nightflyTransparent = 0
-
-  vim.g.nvcode_termcolors = 256
-
   vim.o.background = "dark"
 
   vim.g.tokyonight_style = "night"
