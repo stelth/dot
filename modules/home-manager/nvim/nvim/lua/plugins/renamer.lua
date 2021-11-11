c5576cfb967f95f208fda8df8fd7614ca3fdf066
Jason Cox <steel300@gmail.com>
2021-11-11 15:15:47 -0600

    feature(neovim): Switch homebrewed renamer UI to renamer UI plugin

---
+++ modules/home-manager/nvim/nvim/lua/plugins/renamer.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+local setup = function()
+  require("renamer").setup()
+end
+
+M.use = function(use)
+  use({
+    "filipdutescu/renamer.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = setup,
+  })
+end
+
+return M
