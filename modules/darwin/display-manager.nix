684f658fd7b69cc8659c18870fe439be784a67a6
Jason Cox <steel300@gmail.com>
2021-11-15 14:03:05 -0600

    fix(nix): yabai is still broken on monterey, disable it

--- modules/darwin/display-manager.nix
+++ modules/darwin/display-manager.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }: {
   services.yabai = {
-    enable = true;
+    enable = false;
     package = pkgs.yabai;
     config = {
       mouse_follows_focus = "off";
@@ -31,7 +31,7 @@
   };
 
   services.skhd = {
-    enable = true;
+    enable = false;
     package = pkgs.skhd;
     skhdConfig = builtins.readFile ../home-manager/dotfiles/skhd/skhdrc;
   };
