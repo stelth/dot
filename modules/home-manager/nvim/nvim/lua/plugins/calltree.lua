db833c0a004f90c8a66029405fd190db36dab6e8
Jason Cox <steel300@gmail.com>
2021-11-19 11:01:58 -0600

    fix(neovim): Fix lazy loading for calltree and add the last 2 keymaps

--- modules/home-manager/nvim/nvim/lua/plugins/calltree.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/calltree.lua
@@ -9,6 +9,8 @@ local do_keymaps = function()
     c = {
       t = {
         name = "Tree",
+        c = { "<cmd>CTCollapse<CR>", "Collapse" },
+        e = { "<cmd>CTExpand<CR>", "Expand" },
         i = { "<cmd>lua vim.lsp.buf.incoming_calls()<CR>", "Incoming Calls" },
         o = { "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>", "Outgoing Calls" },
         f = { "<cmd>CTFocus<CR>", "Focus" },
@@ -32,7 +34,7 @@ end)
 M.use = function(use)
   use({
     "ldelossa/calltree.nvim",
-    keys = { "<leader>cti", "<leader>cto" },
+    cmds = { "<cmd>lua vim.lsp.buf.incoming_calls()", "<cmd>lua vim.lsp.buf.outgoing_calls()" },
     config = setup,
   })
 end
