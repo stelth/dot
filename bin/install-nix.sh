a34cace440872b2bb3492bebd80dfce89019993a
Jason Cox <steel300@gmail.com>
2021-12-12 09:54:10 -0600

    chore(nix): Format nix installer

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -4,11 +4,23 @@ RELEASE="nix-2.5pre20211026_5667822"
 URL="https://github.com/numtide/nix-unstable-installer/releases/download/$RELEASE/install"
 
 # install using workaround for darwin systems
-[[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
-[[ -n "$1" ]] && URL="$1"
+if [[ $(uname -s) = "Darwin" ]]; then
+    FLAG="--darwin-use-unencrypted-nix-store-volume"
+fi
 
-if command -v nix >/dev/null; then
-	echo "nix is already installed on this system."
+[[ ! -z "$1" ]] && URL="$1"
+
+if command -v nix > /dev/null; then
+    echo "nix is already installed on this system."
 else
-	bash <(curl -L "$URL") --daemon "$FLAG"
+    bash <(curl -L $URL) --daemon $FLAG
+fi
+
+NIX_CONF_PATH=$HOME/.config/nix
+if [[ ! -d $NIX_CONF_PATH ]]; then
+    mkdir -p $NIX_CONF_PATH
+fi
+
+if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" < $NIX_CONF_PATH; then
+    echo "experimental-features = nix-command flakes" | tee -a $NIX_CONF_PATH/nix.conf
 fi
