3e3c22049d5d0902ffd5f9df799753171de59c9a
Jason Cox <steel300@gmail.com>
2021-12-13 09:04:05 -0600

    feature(neovim): We don't need to change the default setup for lightspeed

--- modules/home-manager/nvim/nvim/lua/plugins/lightspeed.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/lightspeed.lua
@@ -1,16 +1,7 @@
 local M = {}
 
 local setup = function()
-  require("lightspeed").setup({
-    jump_on_partial_input_safety_timeout = 400,
-    highlight_unique_chars = false,
-    grey_out_search_area = true,
-    match_only_the_start_of_same_char_seqs = true,
-    limit_ft_matches = 5,
-    labels = nil,
-    cycle_group_fwd_key = nil,
-    cycle_group_bwd_key = nil,
-  })
+  require("lightspeed").setup({})
 end
 
 M.use = function(use)
