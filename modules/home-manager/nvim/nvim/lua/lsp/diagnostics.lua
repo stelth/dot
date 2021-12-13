013e4ea610b0540f901605012301cc34a2ab83aa
Jason Cox <steel300@gmail.com>
2021-12-13 10:09:32 -0600

    feature(neovim): Better diagnostic icons for bufferline

--- modules/home-manager/nvim/nvim/lua/lsp/diagnostics.lua
+++ modules/home-manager/nvim/nvim/lua/lsp/diagnostics.lua
@@ -1,17 +1,20 @@
--- Automatically update diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
-})
+local M = {}
 
-local function lspSymbol(name, icon)
-  local hl = "DiagnosticSign" .. name
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+M.signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+M.setup = function()
+  -- Automatically update diagnostics
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
+  })
+
+  for type, icon in pairs(M.signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  end
 end
 
-lspSymbol("Error", " ")
-lspSymbol("Warn", " ")
-lspSymbol("Hint", " ")
-lspSymbol("Info", " ")
+return M
--- modules/home-manager/nvim/nvim/lua/lsp/init.lua
+++ modules/home-manager/nvim/nvim/lua/lsp/init.lua
@@ -1,4 +1,4 @@
-require("lsp.diagnostics")
+require("lsp.diagnostics").setup()
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
