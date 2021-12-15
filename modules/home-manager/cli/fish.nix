fc7b3d4a6e5a9f02154fce2c37bea06aecbe26e5
Jason Cox <steel300@gmail.com>
2021-12-15 09:41:51 -0600

    fix(fish): The separate nix plugin isn't needed, home-manager handles that configuration for you

--- modules/home-manager/cli/fish.nix
+++ modules/home-manager/cli/fish.nix
@@ -35,15 +35,6 @@ in {
       hide-cursor = "tput civis";
     };
     functions = { yabai_fix = "pgrep yabai | xargs kill"; };
-    plugins = [{
-      name = "nix-env";
-      src = pkgs.fetchFromGitHub {
-        owner = "lilyball";
-        repo = "nix-env.fish";
-        rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
-        sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
-      };
-    }];
     interactiveShellInit = ''
       ${theme}
     '';
