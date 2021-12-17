6433b8b3350850aa7d3cdd8d7f50bde664751aaf
Jason Cox <steel300@gmail.com>
2021-12-17 12:20:16 -0600

    feature(neovim): Enable repeat f/t in lightspeed

--- modules/home-manager/nvim/nvim/lua/plugins/lightspeed.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/lightspeed.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 local setup = function()
-  require("lightspeed").setup({})
+  require("lightspeed").setup({
+    repeat_ft_with_target_char = true,
+  })
 end
 
 M.use = function(use)
