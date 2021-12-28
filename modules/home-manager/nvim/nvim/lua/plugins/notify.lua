6c849724cb9a4edfeb6be153423821dd4d534133
Jason Cox <steel300@gmail.com>
2021-12-28 08:40:28 -0600

    feature(neovim): Use notify popup plugin. Override vim.notify to ignore the multiple client offset_encodings warning

--- modules/home-manager/nvim/nvim/lua/plugins/notify.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/notify.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 local setup = function()
-  vim.notify = require("notify")
+  vim.notify = function(msg, log_level, opts)
+    if msg:match("offset_encodings") then
+      return
+    end
+    require("notify").notify(msg, log_level, opts)
+  end
 end
 
 M.use = function(use)
