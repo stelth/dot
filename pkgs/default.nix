4682b84294be543aa8103ec3476270b36e72c567
Jason Cox <steel300@gmail.com>
2022-01-03 20:42:32 -0600

    fix(nix): Delete the call to include the prettierd derivation

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 self: super: {
-  nodePackages = super.nodePackages // super.callPackage ./prettierd { };
   selene = super.callPackage ./selene { };
   jdt-language-server = super.callPackage ./jdt-language-server { };
 }
