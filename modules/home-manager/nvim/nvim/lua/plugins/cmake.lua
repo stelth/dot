9b56049719a2b9fc222aa3f6018443473431ddea
Jason Cox <steel300@gmail.com>
2022-01-12 08:49:37 -0600

    fix(neovim): cmake-neovim upstream uses vim.ui instead of telescope directly, adapt

--- modules/home-manager/nvim/nvim/lua/plugins/cmake.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/cmake.lua
@@ -6,8 +6,6 @@ local setup = function()
     dap_open_command = require("dapui").open,
   })
 
-  require("telescope").load_extension("cmake")
-
   vim.api.nvim_set_keymap("n", "<leader>cbba", ":CMake build_all<CR>", { desc = "Build all" })
   vim.api.nvim_set_keymap("n", "<leader>cbbd", ":CMake build_and_debug<CR>", { desc = "Build and debug" })
   vim.api.nvim_set_keymap("n", "<leader>cbbr", ":CMake build_and_run<CR>", { desc = "Build and run" })
@@ -18,8 +16,8 @@ local setup = function()
   vim.api.nvim_set_keymap("n", "<leader>cbk", ":CMake cancel<CR>", { desc = "Cancel build" })
   vim.api.nvim_set_keymap("n", "<leader>cbr", ":CMake run<CR>", { desc = "Run target" })
   vim.api.nvim_set_keymap("n", "<leader>cbsa", ":CMake set_target_args<CR>", { desc = "Set arguments" })
-  vim.api.nvim_set_keymap("n", "<leader>cbsb", ":Telescope cmake select_build_type<CR>", { desc = "Select build type" })
-  vim.api.nvim_set_keymap("n", "<leader>cbst", ":Telescope cmake select_target<CR>", { desc = "Select target" })
+  vim.api.nvim_set_keymap("n", "<leader>cbsb", ":CMake select_build_type<CR>", { desc = "Select build type" })
+  vim.api.nvim_set_keymap("n", "<leader>cbst", ":CMake select_target<CR>", { desc = "Select target" })
 end
 
 M.use = function(use)
