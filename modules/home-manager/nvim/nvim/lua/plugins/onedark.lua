71830465dd2f1949d6af05acc93423506cc13b71
Jason Cox <steel300@gmail.com>
2022-01-15 22:04:32 -0600

    feat(neovim): Add color highlighting for lightspeed

--- modules/home-manager/nvim/nvim/lua/plugins/onedark.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/onedark.lua
@@ -6,9 +6,22 @@ local setup = function()
     dark_sidebar = false,
     highlight_linenumber = true,
     hide_inactive_statusline = true,
-
     comment_style = "italic",
     keyword_style = "italic",
+    overrides = function(c)
+      return {
+        LightspeedGreyWash = { fg = c.dark3 },
+        LightspeedLabel = { fg = c.magenta2, style = "bold,underline" },
+        LightspeedLabelDistant = { fg = c.green1, style = "bold,underline" },
+        LightspeedLabelDistantOverlapped = { fg = c.green2, style = "underline" },
+        LightspeedLabelOverlapped = { fg = c.magenta2, style = "underline" },
+        LightspeedMaskedChar = { fg = c.orange },
+        LightspeedOneCharMatch = { bg = c.magenta2, fg = c.fg, style = "bold" },
+        LightspeedPendingOpArea = { bg = c.magenta2, fg = c.fg },
+        LightspeedShortcut = { bg = c.magenta2, fg = c.fg, style = "bold,underline" },
+        LightspeedUnlabeledMatch = { fg = c.blue2, style = "bold" },
+      }
+    end,
   })
 end
 
