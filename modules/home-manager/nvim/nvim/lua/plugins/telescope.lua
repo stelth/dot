f4a44b43d7ffaaf06d75ce31ac5b5d90409f9d8d
Jason Cox <steel300@gmail.com>
2022-01-18 09:57:45 -0600

    Revert "feat(neovim): Update telescope config"
    This reverts commit 8ab3c888064e9392502882c43d2813306073510d.

--- modules/home-manager/nvim/nvim/lua/plugins/telescope.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/telescope.lua
@@ -4,63 +4,13 @@ local setup = function()
   local telescope = require("telescope")
 
   telescope.setup({
+    extensions = { fzy_native = { override_generic_sorter = false, override_file_sorter = true } },
     defaults = {
-      prompt_prefix = "❯ ",
-      selection_caret = "❯ ",
-
-      winblend = 0,
-
-      layout_strategy = "horizontal",
+      prompt_prefix = " ",
+      selection_caret = " ",
+      winblend = 10,
       layout_config = {
-        width = 0.95,
-        height = 0.85,
-        prompt_position = "bottom",
-
-        horizontal = {
-          preview_width = function(_, cols, _)
-            if cols > 200 then
-              return math.floor(cols * 0.4)
-            else
-              return math.floor(cols * 0.6)
-            end
-          end,
-        },
-
-        vertical = {
-          width = 0.9,
-          height = 0.95,
-          preview_height = 0.5,
-        },
-
-        flex = {
-          horizontal = {
-            preview_width = 0.9,
-          },
-        },
-      },
-
-      selection_strategy = "reset",
-      sorting_strategy = "descending",
-      scroll_strategy = "cycle",
-      color_devicons = true,
-
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-    },
-
-    git_branches = {
-      mappings = {
-        i = {
-          ["<C-a>"] = false,
-        },
-      },
-    },
-
-    extensions = {
-      fzy_native = {
-        override_generic_sorter = true,
-        override_file_sorter = true,
+        width = 0.7,
       },
     },
   })
