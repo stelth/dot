d3e4b590a77d88d3a224a389cee5fc90db0372c5
Jason Cox <steel300@gmail.com>
2022-02-08 12:24:07 -0600

    chore(nix): Run treefmt on the source

--- modules/home-manager/nvim/nvim/lua/plugins/nvim-hlslens.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/nvim-hlslens.lua
@@ -1,18 +1,8 @@
 local M = {}
 
 local setup = function()
-  vim.keymap.set(
-    "n",
-    "n",
-    "<cmd>execute('normal!' . v:count1 . 'n')<CR><cmd>lua require('hlslens').start()<CR>",
-    {}
-  )
-  vim.keymap.set(
-    "n",
-    "N",
-    "<cmd>execute('normal!' . v:count1 . 'N')<CR><cmd>lua require('hlslens').start()<CR>",
-    {}
-  )
+  vim.keymap.set("n", "n", "<cmd>execute('normal!' . v:count1 . 'n')<CR><cmd>lua require('hlslens').start()<CR>", {})
+  vim.keymap.set("n", "N", "<cmd>execute('normal!' . v:count1 . 'N')<CR><cmd>lua require('hlslens').start()<CR>", {})
   vim.keymap.set("n", "*", "*<cmd>lua require('hlslens').start()<CR>", {})
   vim.keymap.set("n", "#", "#<cmd>lua require('hlslens').start()<CR>", {})
   vim.keymap.set("n", "g*", "g*<cmd>lua require('hlslens).start()<CR>", {})
