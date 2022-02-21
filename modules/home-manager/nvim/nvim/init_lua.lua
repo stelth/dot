f9c139429ee204d1427bdf5f7424973201f778e0
Jason Cox <steel300@gmail.com>
2022-02-21 00:02:26 -0600

    feat(neovim): Use the builtin filetype.lua

--- modules/home-manager/nvim/nvim/init_lua.lua
+++ modules/home-manager/nvim/nvim/init_lua.lua
@@ -64,6 +64,9 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
+vim.g.did_load_filetypes = 0
+
+vim.g.do_filetype_lua = 1
 
 pcall(require, "packer_compiled")
 
