c65d5317c437d06dba63b60d83b8627b6486dc92
Jason Cox <steel300@gmail.com>
2022-03-30 14:02:34 -0500

    feat(nix): Upstream packages sumneko-lua-language-server that works fine, no need to override parts of it

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -4,43 +4,6 @@
     inputs.neovim-nightly-overlay.overlay
     (import ../pkgs)
     (final: prev: rec {
-      sumneko-lua-language-server =
-        prev.sumneko-lua-language-server.overrideAttrs (o: rec {
-          version = "2.6.7";
-
-          src = builtins.fetchurl {
-            url =
-              "https://github.com/sumneko/vscode-lua/releases/download/v${version}/vscode-lua-v${version}-darwin-x64.vsix";
-            sha256 = "0f5g4s3ipr57gbd94sh9asjcssyyc85vyi8fv5vwd68i7sc41b13";
-          };
-
-          unpackPhase = ''
-            ${prev.pkgs.unzip}/bin/unzip $src
-          '';
-
-          postPatch = "";
-
-          preBuild = "";
-          postBuild = "";
-          nativeBuildInputs = [ prev.makeWrapper ];
-
-          dontPatch = true;
-
-          installPhase = ''
-            mkdir -p $out
-            cp -r extension $out/extras
-            chmod a+x $out/extras/server/bin/lua-language-server
-            makeWrapper $out/extras/server/bin/lua-language-server \
-            $out/bin/lua-language-server \
-            --add-flags "-E -e LANG=en $out/extras/server/main.lua \
-            --logpath='~/.cache/sumneko_lua/log' \
-            --metapath='~/.cache/sumneko_lua/meta'"
-          '';
-
-          meta.platforms = prev.lib.platforms.all;
-        });
-    })
-    (final: prev: rec {
       lldb = prev.lldb.overrideAttrs (old: {
         patches = (old.patches or [ ])
           ++ [ ./patches/lldb-fix-cpu-subtype-not-found.patch ];
