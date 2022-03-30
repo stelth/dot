e7c3982b70e2f079927298eeb317af073fc49319
Jason Cox <steel300@gmail.com>
2022-03-30 10:29:12 -0500

    chore(nix): Remove yabai overlay, we use Rectangle instead

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -41,30 +41,6 @@
         });
     })
     (final: prev: rec {
-      yabai = let
-        buildSymlinks = prev.runCommand "build-symlinks" { } ''
-          mkdir -p $out/bin
-          ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
-        '';
-      in prev.yabai.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "koekeishiya";
-          repo = "yabai";
-          rev = "34d31e8c1b6c0969dc4181f521aa2edb8757b805";
-          sha256 = "sha256-jX4i+VpLNiRuUfwJE8ys6uMaoHjjb1sa+GyMjTpjCVk=";
-        };
-        buildInputs = with prev.darwin.apple_sdk.frameworks; [
-          Carbon
-          Cocoa
-          ScriptingBridge
-          prev.xxd
-          SkyLight
-        ];
-        nativeBuildInputs = [ buildSymlinks ];
-      });
-
-    })
-    (final: prev: rec {
       lldb = prev.lldb.overrideAttrs (old: {
         patches = (old.patches or [ ])
           ++ [ ./patches/lldb-fix-cpu-subtype-not-found.patch ];
