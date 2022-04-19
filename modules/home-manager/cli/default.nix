cce8d8763cd1794f740ac8e6fe8b6d4fbf9c5888
Jason Cox <steel300@gmail.com>
2022-04-19 15:49:11 -0500

    fix(nix): Starship build is unbroke on darwin, go back to unstable

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -52,7 +52,6 @@ in {
     zoxide.enable = true;
     starship = {
       enable = true;
-      package = pkgs.stable.starship;
       enableFishIntegration = true;
       settings = {
         add_newline = true;
