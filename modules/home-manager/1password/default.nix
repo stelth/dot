2a9cbf7ba4d472ffbcffc790eeb61bf4b5dab1c0
Jason Cox <steel300@gmail.com>
2022-04-20 09:48:56 -0500

    chore(nix): Cleanup 1password config

--- modules/home-manager/1password/default.nix
+++ modules/home-manager/1password/default.nix
@@ -1,11 +1,14 @@
 { config, lib, pkgs, ... }:
-let sockPath = "~/.1password/agent.sock";
+let
+  home = config.home.homeDirectory;
+  darwinSockPath =
+    "${home}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  sockPath = "~/.1password/agent.sock";
 in {
-  home.sessionVariables.SSH_AUTH_SOCK = "${sockPath}";
+  home.sessionVariables.SSH_AUTH_SOCK = sockPath;
   home.file = {
     sock = lib.mkIf pkgs.stdenvNoCC.isDarwin {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      source = config.lib.file.mkOutOfStoreSymlink darwinSockPath;
       target = ".1password/agent.sock";
     };
     sshConfig = {
