25a296612daf3fb5c221eac66099228208803e7b
Jason Cox <steel300@gmail.com>
2022-05-18 09:43:48 -0500

    fix(ssh): Use the correct ssh key for the 172.19.* build node

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -26,13 +26,14 @@
       };
       "github.com" =
         lib.hm.dag.entryBefore [ "10.* 172.* 192.*" ] { user = "stelth"; };
-      "10.137.25.4 10.137.26.4" = lib.hm.dag.entryBefore [ "github.com" ] {
-        user = "root";
-        identitiesOnly = true;
-        identityFile = "~/.ssh/keys/id_rsa.build_node";
-        serverAliveInterval = 50;
-        extraOptions = { "StrictHostKeyChecking" = "no"; };
-      };
+      "10.137.25.4 10.137.26.4 172.19.22.112" =
+        lib.hm.dag.entryBefore [ "github.com" ] {
+          user = "root";
+          identitiesOnly = true;
+          identityFile = "~/.ssh/keys/id_rsa.build_node";
+          serverAliveInterval = 50;
+          extraOptions = { "StrictHostKeyChecking" = "no"; };
+        };
       "github.ibm.com" = lib.hm.dag.entryBefore [ "github.com" ] {
         user = "Jason.P.Cox@ibm.com";
       };
