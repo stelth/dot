7a4bbace7054df13a8939ceeb3e9af50f7df21fc
Jason Cox <steel300@gmail.com>
2022-06-01 13:36:57 -0500

    chore(nix): remove lldb until upstream fixes it

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -6,11 +6,5 @@
     })
     inputs.neovim-nightly-overlay.overlay
     (import ../pkgs)
-    (final: prev: rec {
-      lldb = prev.lldb.overrideAttrs (old: {
-        patches = (old.patches or [ ])
-          ++ [ ./patches/lldb-fix-cpu-subtype-not-found.patch ];
-      });
-    })
   ];
 }
