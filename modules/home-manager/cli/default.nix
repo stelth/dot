a13afa492b2af0030495597299372421ead7a05d
Jason Cox <steel300@gmail.com>
2022-06-07 11:57:07 -0500

    chore(kitty): Remove unnecessary starship symbols

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -53,59 +53,7 @@ in {
     starship = {
       enable = true;
       enableFishIntegration = true;
-      settings = {
-        add_newline = true;
-
-        aws = { symbol = " "; };
-
-        battery = {
-          full_symbol = "";
-          charging_symbol = "";
-          discharging_symbol = "";
-        };
-
-        conda = { symbol = " "; };
-
-        dart = { symbol = " "; };
-
-        docker_context = { symbol = " "; };
-
-        elixir = { symbol = " "; };
-
-        elm = { symbol = " "; };
-
-        git_branch = { symbol = " "; };
-
-        golang = { symbol = " "; };
-
-        hg_branch = { symbol = " "; };
-
-        java = { symbol = " "; };
-
-        julia = { symbol = " "; };
-
-        memory_usage = { symbol = " "; };
-
-        nim = { symbol = " "; };
-
-        nix_shell = { symbol = " "; };
-
-        nodejs = { symbol = " "; };
-
-        package = { symbol = " "; };
-
-        perl = { symbol = " "; };
-
-        php = { symbol = " "; };
-
-        python = { symbol = " "; };
-
-        ruby = { symbol = " "; };
-
-        rust = { symbol = " "; };
-
-        swift = { symbol = "ﯣ "; };
-      };
+      settings = { add_newline = true; };
     };
   };
 }
