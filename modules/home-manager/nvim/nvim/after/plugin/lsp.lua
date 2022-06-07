7237effd7e0c4d49734cc63ea8b8b47e9b45ea44
Jason Cox <steel300@gmail.com>
2022-06-07 10:19:30 -0500

    chore(neovim): Remove custom diagnostic signs, they're hard to read

--- modules/home-manager/nvim/nvim/after/plugin/lsp.lua
+++ modules/home-manager/nvim/nvim/after/plugin/lsp.lua
@@ -1,5 +1,3 @@
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-
 -- Automatically update diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
@@ -7,11 +5,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   virtual_text = { spacing = 4, prefix = "●" },
   severity_sort = true,
 })
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
 
 local autoformat = true
 
