ba0d1f4b52edbfdffefc27a4db9b8c68181720b5
Jason Cox <steel300@gmail.com>
2022-06-08 11:10:30 -0500

    chore(tmux): Remove unused tmux plugins and config options

--- modules/home-manager/cli/tmux.nix
+++ modules/home-manager/cli/tmux.nix
@@ -8,12 +8,7 @@
     customPaneNavigationAndResize = true;
     disableConfirmationPrompt = true;
     escapeTime = 0;
-    tmuxinator.enable = true;
-    plugins = with pkgs; [ tmuxPlugins.yank ];
     extraConfig = ''
-      set -g mouse on
-      set -g renumber-windows on
-      setw -g monitor-activity on
       bind | split-window -h
       bind - split-window -v
       set -g status-style 'bg=#333333 fg=#5EACD3'
