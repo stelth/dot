06f0b5171f4942f096709b98777835d48b0bb32e
Jason Cox <steel300@gmail.com>
2022-06-14 08:53:14 -0500

    chore(hammerspoon): Redo keybindings for new Ergodox keyboard

--- modules/home-manager/dotfiles/hammerspoon/shortcuts.lua
+++ modules/home-manager/dotfiles/hammerspoon/shortcuts.lua
@@ -1,20 +1,62 @@
-local hyper = require("hyper")
+local hyper = { "ctrl", "alt", "shift", "cmd" }
+local meh = { "ctrl", "alt", "shift" }
 
-hyper.bindApp({}, "f", "Firefox")
-hyper.bindApp({ "cmd" }, "f", function()
-  hs.osascript.javascript([[
+local keymaps = {
+  ["Google Chrome"] = {
+    mods = hyper,
+    key = "b",
+  },
+  ["Google Chrome New Window"] = {
+    mods = meh,
+    key = "b",
+    callback = function()
+      hs.osascript.javascript([[
         Application("Firefox").Window().make()
     ]])
-end)
-hyper.bindApp({}, "b", "Google Chrome")
-hyper.bindApp({ "cmd" }, "b", function()
-  hs.osascript.javascript([[
-        Application("Google Chrome").Window().make()
-    ]])
-end)
+    end,
+  },
+  ["Firefox"] = {
+    mods = hyper,
+    key = "f",
+  },
+  ["Firefox New Window"] = {
+    mods = meh,
+    key = "f",
+    callback = function()
+      hs.osascript.javascript([[
+         Application("Firefox").Window().make()
+     ]])
+    end,
+  },
+  ["kitty"] = {
+    mods = hyper,
+    key = "k",
+  },
+  ["Microsoft Outlook"] = {
+    mods = hyper,
+    key = "o",
+  },
+  ["Slack"] = {
+    mods = hyper,
+    key = "s",
+  },
+  ["Discord"] = {
+    mods = hyper,
+    key = "d",
+  },
+  ["Cyberduck"] = {
+    mods = hyper,
+    key = "c",
+  },
+}
 
-hyper.bindApp({}, "k", "kitty")
-hyper.bindApp({}, "o", "Microsoft Outlook")
-hyper.bindApp({}, "s", "Slack")
-hyper.bindApp({}, "d", "Discord")
-hyper.bindApp({}, "c", "Cyberduck")
+for name, options in pairs(keymaps) do
+  local fn = function()
+    hs.application.launchOrFocus(name)
+  end
+
+  if options.callback ~= nil then
+    fn = options.callback
+  end
+  hs.hotkey.bind(options.mods or {}, options.key, fn)
+end
