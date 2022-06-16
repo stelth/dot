2da7addbadf56129f7c55475bf3044e0c986ee21
Jason Cox <steel300@gmail.com>
2022-06-16 08:44:57 -0500

    feat(nix): Make dev tools available globally

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -63,47 +63,5 @@ in {
       undotree
       vim-matchup
     ];
-
-    extraPackages = with pkgs; [
-      # Essentials
-      tree-sitter
-      fd
-      fzy
-
-      # C/C++
-      clang-tools_14
-      cppcheck
-      lldb_14
-
-      # Java
-      jdt-language-server
-
-      #nix
-      nixfmt
-      rnix-lsp
-      statix
-
-      # python
-      (python3.withPackages (ps: with ps; [ autopep8 flake8 isort yamllint ]))
-      nodePackages.pyright
-
-      # Lua
-      selene
-      stylua
-      sumneko-lua-language-server
-
-      # Shell scripting
-      nodePackages.bash-language-server
-      shellcheck
-      shellharden
-      shfmt
-
-      # Additional
-      cmake-language-server
-      nodePackages.markdownlint-cli
-      nodePackages.prettier
-      nodePackages.vscode-json-languageserver
-      nodePackages.yaml-language-server
-    ];
   };
 }
