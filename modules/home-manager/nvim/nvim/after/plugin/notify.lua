92cd1a7133301280076594d8e411ac7a9957015c
Jason Cox <steel300@gmail.com>
2022-06-20 21:14:00 -0500

    fix(neovim): Don't continually show the "multiple client offset_encodings" message

--- modules/home-manager/nvim/nvim/after/plugin/notify.lua
+++ modules/home-manager/nvim/nvim/after/plugin/notify.lua
@@ -1,1 +1,6 @@
-vim.notify = require("notify")
+vim.notify = function(msg, log_level, opts)
+  if msg:match("offset_encodings") then
+    return
+  end
+  require("notify").notify(msg, log_level, opts)
+end
