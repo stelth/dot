8429749da0a0531895ec7fea75e3f68ce998a09d
Jason Cox <steel300@gmail.com>
2022-07-05 21:39:00 -0500

    chore(nix): Update leap.nvim plugin

--- modules/home-manager/nvim/plugins/leap/default.nix
+++ modules/home-manager/nvim/plugins/leap/default.nix
@@ -6,8 +6,8 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "ggandor";
       repo = "leap.nvim";
-      rev = "1ab9deb88ef8ccf01a359ef5053b7d3804f8425a";
-      sha256 = "sha256-UuZvMxBt1FRnKAUo/G5HBLDkRcyQiQhlN+xJ9ti3+Wg=";
+      rev = "a6072a8e1ed3d655e214721fc804c1eae508665e";
+      sha256 = "sha256-eGatUG9aPSYRHVsp/6CH6JlCXDeDen6rw7SnoZ9SdGc=";
     };
     meta.homepage = "https://github.com/ggandor/leap.nvim";
   };
