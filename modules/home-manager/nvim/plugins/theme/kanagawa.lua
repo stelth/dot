dc4754649768df4faa39616cf6ec52fe535040c7
Jason Cox <steel300@gmail.com>
2022-07-05 21:32:52 -0500

    fix(neovim): Fix leap coloring so its easier to read

--- modules/home-manager/nvim/plugins/theme/kanagawa.lua
+++ modules/home-manager/nvim/plugins/theme/kanagawa.lua
@@ -4,9 +4,23 @@ require("kanagawa").setup({
   dimInactive = true,
   globalStatus = true,
   overrides = {
-    LeapLabelPrimary = { fg = default_colors.autumnRed },
-    LeapLabelSecondary = { fg = default_colors.autumnRed },
-    LeapMatch = { fg = default_colors.waveRed, italic = true },
+    LeapLabelPrimary = {
+      bg = default_colors.sumiInk1,
+      fg = default_colors.springGreen,
+    },
+    LeapLabelSecondary = {
+      bg = default_colors.sumiInk1,
+      fg = default_colors.springBlue,
+    },
+    LeapMatch = {
+      bg = default_colors.sumiInk1,
+      fg = default_colors.waveRed,
+      italic = true,
+    },
+    LeapBackdrop = {
+      bg = default_colors.sumiInk1,
+      fg = default_colors.sumiInk4,
+    },
   },
 })
 
