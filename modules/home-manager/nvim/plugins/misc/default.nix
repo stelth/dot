deeb34e9ff986984134fd8cb0dd5a29f33a18ceb
Jason Cox <steel300@gmail.com>
2022-07-07 15:05:56 -0500

    feat(neovim): Add nvim-surround plugin

--- modules/home-manager/nvim/plugins/misc/default.nix
+++ modules/home-manager/nvim/plugins/misc/default.nix
@@ -1,4 +1,17 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  nvim-surround = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    name = "nvim-surround";
+    version = "2022-07-07";
+    src = pkgs.fetchFromGitHub {
+      owner = "kylechui";
+      repo = "nvim-surround";
+      rev = "1e308812e9bf9b98c4cf5fa7500b569385a19f96";
+      sha256 = "sha256-dgC1ZdV/IWEYQTn66jDFF2H71iTZYwZMLrzgYrFBFcc=";
+    };
+    meta.homepage = "https://github.com/kylechui/nvim-surround";
+  };
+in {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       (config.lib.vimUtils.pluginWithCfg {
@@ -21,6 +34,10 @@
       (config.lib.vimUtils.pluginWithCfg {
         plugin = nvim-notify;
         file = ./notify.lua;
+      })
+      (config.lib.vimUtils.pluginWithCfg {
+        plugin = nvim-surround;
+        file = ./surround.lua;
       })
       (config.lib.vimUtils.pluginWithCfg {
         plugin = undotree;
