a05ff8b5da0641d7ec7bed0a43b85f019cda26fc
Jason Cox <steel300@gmail.com>
2022-07-11 09:25:04 -0500

    feat(neovim): The filetype lua code is default now, doing it manually breaks ft detection

--- modules/home-manager/nvim/settings.lua
+++ modules/home-manager/nvim/settings.lua
@@ -36,7 +36,6 @@ vim.opt.updatetime = 50
 vim.opt.wrap = false
 
 -- don't load the plugins below
-vim.g.did_load_filetypes = 0
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_fzf = 1
 vim.g.loaded_getscript = 1
@@ -55,8 +54,6 @@ vim.g.loaded_vimball = 1
 vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
-
-vim.g.do_filetype_lua = 1
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
