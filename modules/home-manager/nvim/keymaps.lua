f0ffea9486c30b83af1da1e3e9b3ecc15660c7cb
Jason Cox <steel300@gmail.com>
2022-07-21 11:22:48 -0500

    feat(neovim): Add keymap to move selected lines up and down

--- modules/home-manager/nvim/keymaps.lua
+++ modules/home-manager/nvim/keymaps.lua
@@ -32,6 +32,9 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("x", "<leader>p", '"_dp')
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y"')
 vim.keymap.set("n", "<leader>Y", '"+Y"')
