80c18a52cceb5cd32277197642f4eeb676c8c4f7
Jason Cox <steel300@gmail.com>
2022-08-04 23:16:00 -0500

    fix(tmux-sessionizer): Use the entire repo name for the session name

--- pkgs/tmux-sessionizer/default.nix
+++ pkgs/tmux-sessionizer/default.nix
@@ -19,21 +19,21 @@ writeShellApplication {
     fi
 
     selected_name=$(basename "$selected" | tr . _)
-    selected_name_t=''${selected_name:0:8}
+    # selected_name_t=''${selected_name:0:8}
 
     # If you are in a tmux session, and the selected session exists, switch to it;
     # if not create a new one and then swith to it.
     if [[ -n ''${TMUX+x} ]]; then
-    	if ! tmux switch-client -t "$selected_name_t"; then
-    		if tmux new-session -ds "$selected_name_t" -c "$selected"; then
-    			tmux switch-client -t "$selected_name_t"
+    	if ! tmux switch-client -t "$selected_name"; then
+    		if tmux new-session -ds "$selected_name" -c "$selected"; then
+    			tmux switch-client -t "$selected_name"
     		fi
     	fi
     # If outside of a tmux session, try to create a new session;
     # if it fails attach to the selected session
     else
-    	if ! tmux new-session -s "$selected_name_t" -c "$selected"; then
-    		tmux attach -t "$selected_name_t"
+    	if ! tmux new-session -s "$selected_name" -c "$selected"; then
+    		tmux attach -t "$selected_name"
     	fi
     fi
   '';
