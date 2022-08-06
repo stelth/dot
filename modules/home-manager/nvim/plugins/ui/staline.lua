d161313af0f5fe7fabf89b886c815aee3be3894a
Jason Cox <steel300@gmail.com>
2022-08-05 22:08:13 -0500

    chore(neovim): Format staline.lua

--- modules/home-manager/nvim/plugins/ui/staline.lua
+++ modules/home-manager/nvim/plugins/ui/staline.lua
@@ -1,12 +1,12 @@
 require("staline").setup({
-    sections = {
-        left = { "  ", "mode", " ", "branch", " ", "lsp" },
-        mid = {},
-        right = { "file_name", "line_column" },
-    },
-    defaults = {
-        true_colors = true,
-        line_column = " [%l/%L] :%c  ",
-        branch_symbol = " ",
-    },
+  sections = {
+    left = { "  ", "mode", " ", "branch", " ", "lsp" },
+    mid = {},
+    right = { "file_name", "line_column" },
+  },
+  defaults = {
+    true_colors = true,
+    line_column = " [%l/%L] :%c  ",
+    branch_symbol = " ",
+  },
 })
