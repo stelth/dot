ba064d83265219fd32154dad2edd926e9ca96d65
Jason Cox <steel300@gmail.com>
2022-08-05 22:18:44 -0500

    fix(nix): We don't need to explicitly pass pkgs into the vim plugin derivations

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,14 +5,9 @@ self: super: {
   tmux-sessionizer = super.callPackage ./tmux-sessionizer { };
 
   vimPlugins = super.vimPlugins // {
-    fine-cmdline-nvim = super.callPackage ./vimPlugins/fine-cmdline-nvim {
-      inherit (super) pkgs;
-    };
-    leap-nvim =
-      super.callPackage ./vimPlugins/leap-nvim { inherit (super) pkgs; };
-    nvim-surround =
-      super.callPackage ./vimPlugins/nvim-surround { inherit (super) pkgs; };
-    staline-nvim =
-      super.callPackage ./vimPlugins/staline-nvim { inherit (super) pkgs; };
+    fine-cmdline-nvim = super.callPackage ./vimPlugins/fine-cmdline-nvim { };
+    leap-nvim = super.callPackage ./vimPlugins/leap-nvim { };
+    nvim-surround = super.callPackage ./vimPlugins/nvim-surround { };
+    staline-nvim = super.callPackage ./vimPlugins/staline-nvim { };
   };
 }
