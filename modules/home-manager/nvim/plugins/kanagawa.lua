2cfe2159052c0b862e8df2606b46ed75d6ff8945
Jason Cox <steel300@gmail.com>
2022-08-14 19:29:55 -0500

    chore(neovim): Cleanup kanagawa config since we dropped leap and left an unused import

--- modules/home-manager/nvim/plugins/kanagawa.lua
+++ modules/home-manager/nvim/plugins/kanagawa.lua
@@ -1,8 +1,6 @@
-local default_colors = require("kanagawa.colors").setup()
-
 require("kanagawa").setup({
-    dimInactive = true,
-    globalStatus = true,
+  dimInactive = true,
+  globalStatus = true,
 })
 
 vim.cmd.colorscheme("kanagawa")
