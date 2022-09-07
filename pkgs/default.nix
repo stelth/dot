6b2675420fdbb5021d2374c8a7cc89a29ffed520
Jason Cox <steel300@gmail.com>
2022-09-07 13:19:00 -0500

    chore(nix): Remove vestigial default.nix from pkgs/ since flake.nix is handling the overlays

--- pkgs/default.nix
+++
@@ -1,6 +0,0 @@
-self: super: {
-  neocmakelsp = super.callPackage ./neocmakelsp { };
-  switch-back-to-nvim = super.callPackage ./switch-back-to-nvim { };
-  tmux-cht = super.callPackage ./tmux-cht { };
-  tmux-sessionizer = super.callPackage ./tmux-sessionizer { };
-}
