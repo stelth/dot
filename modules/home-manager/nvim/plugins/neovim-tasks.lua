9e253d3c2799318f5715fa98a44ced9a59f9a643
Jason Cox <steel300@gmail.com>
2022-09-19 15:38:50 -0500

    feat(neovim): Mac doesn't handle F keys above 20

--- modules/home-manager/nvim/plugins/neovim-tasks.lua
+++ modules/home-manager/nvim/plugins/neovim-tasks.lua
@@ -5,13 +5,8 @@ tasks.setup({
   dap_open_command = dapui.open,
 })
 
-vim.keymap.set({ "", "i" }, "<C-BS>", function()
-  tasks.cancel()
-  dapui.close()
-end, {})
-
 local f_index = 13
-for _, task_name in ipairs({ "debug", "run", "test", "build" }) do
+for _, task_name in ipairs({ "debug", "run", "build" }) do
   vim.keymap.set({ "", "i" }, string.format("<F%d>", f_index), function()
     tasks.start("auto", task_name)
   end, {})
@@ -24,22 +19,23 @@ for _, task_name in ipairs({ "debug", "run", "test", "build" }) do
   f_index = f_index + 1
 end
 
-vim.keymap.set({ "", "i" }, "<F17>", function()
+vim.keymap.set({ "", "i" }, "<F16>", function()
   tasks.set_module_param("auto", "target")
 end, {})
 
-vim.keymap.set({ "", "i" }, "<F18>", function()
+vim.keymap.set({ "", "i" }, "<F17>", function()
   tasks.set_module_param("auto", "build")
 end, {})
 
-vim.keymap.set({ "", "i" }, "<F19>", function()
+vim.keymap.set({ "", "i" }, "<F18>", function()
   tasks.start("auto", "configure")
 end, {})
 
-vim.keymap.set({ "", "i" }, "<F20>", function()
+vim.keymap.set({ "", "i" }, "<F19>", function()
   tasks.start("auto", "clean")
 end, {})
 
-vim.keymap.set({ "", "i" }, "<F21>", function()
-  tasks.start("auto", "clippy")
+vim.keymap.set({ "", "i" }, "<F20>", function()
+  tasks.cancel()
+  dapui.close()
 end, {})
