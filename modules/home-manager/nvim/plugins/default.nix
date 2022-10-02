23763980e538204722b7aa6db6838d8b321b1b2a
Jason Cox <steel300@gmail.com>
2022-10-01 20:44:51 -0500

    chore(nix): The config for luasnip is small enough, it doesn't need t be its own file

--- modules/home-manager/nvim/plugins/default.nix
+++ modules/home-manager/nvim/plugins/default.nix
@@ -95,7 +95,15 @@ in {
       '')
       lspkind-nvim
       lua-dev-nvim
-      (pluginWithCfgFile luasnip)
+      (pluginWithCfg luasnip ''
+        require("luasnip").config.set_config({
+          history = false,
+          -- Update more often
+          updateEvents = "TextChanged,TextChangedI",
+        })
+
+        require("luasnip/loaders/from_vscode").lazy_load()
+      '')
       (pluginWithCfgFile pkgs.vimExtraPlugins.neovim-tasks)
       null-ls-nvim
       (pluginWithCfg nvim-autopairs ''
