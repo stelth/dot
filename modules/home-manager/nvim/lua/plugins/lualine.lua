b91455f0231c53682e8b83d621d14ab635616866
Jason Cox <steel300@gmail.com>
2022-11-28 15:05:19 -0600

    feat(neovim): Better lualine/noice settings

--- modules/home-manager/nvim/lua/plugins/lualine.lua
+++ modules/home-manager/nvim/lua/plugins/lualine.lua
@@ -16,10 +16,6 @@ require("lualine").setup({
     },
     lualine_x = {
       {
-        require("noice").api.status.message.get_hl,
-        cond = require("noice").api.status.message.has,
-      },
-      {
         require("noice").api.status.command.get,
         cond = require("noice").api.status.command.has,
         color = { fg = "#ff9e64" },
