2bf7acca1eb616a5e8ef4c93df6ef7b516007cef
Jason Cox <steel300@gmail.com>
2022-11-29 14:37:00 -0600

    feat(github): Update CI to leverage multi core builds

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -10,11 +10,11 @@ else
 	bash <(curl -L "$URL") --daemon
 fi
 
-NIX_CONF_PATH=$HOME/.config/nix
+NIX_CONF_PATH=/etc/nix
 if [[ ! -d $NIX_CONF_PATH ]]; then
 	mkdir -p "$NIX_CONF_PATH"
 fi
 
 if [[ ! -f $NIX_CONF_PATH/nix.conf ]] || ! grep "experimental-features" <"$NIX_CONF_PATH"; then
-	echo "experimental-features = nix-command flakes" | tee -a "$NIX_CONF_PATH"/nix.conf
+	echo "experimental-features = nix-command flakes" | sudo tee -a "$NIX_CONF_PATH"/nix.conf
 fi
