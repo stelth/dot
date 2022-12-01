3b057f3f2857aa5cc7dc663ab83e3b015dc172ad
Jason Cox <steel300@gmail.com>
2022-12-01 16:03:58 -0600

    fix(neovim): DAP fixes

--- modules/home-manager/nvim/lua/plugins/nvim-dap.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-dap.lua
@@ -14,6 +14,20 @@ dap.adapters.lldb = {
   name = "lldb",
 }
 
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+
 require("dap-python").setup("/etc/profiles/per-user/coxj/bin/python")
 
 dapui.setup({
@@ -63,12 +77,4 @@ vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open({})
-end
-
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close({})
-end
-
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close({})
 end
