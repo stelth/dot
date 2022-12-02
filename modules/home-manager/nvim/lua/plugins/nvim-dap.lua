fa6d7d5c1c185ed4109c92cd9f217f42e43231bf
Jason Cox <steel300@gmail.com>
2022-12-02 14:37:39 -0600

    feat(neovim): Auto close dapui on program close/stop

--- modules/home-manager/nvim/lua/plugins/nvim-dap.lua
+++ modules/home-manager/nvim/lua/plugins/nvim-dap.lua
@@ -78,3 +78,11 @@ vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open({})
 end
+
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close({})
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close({})
+end
