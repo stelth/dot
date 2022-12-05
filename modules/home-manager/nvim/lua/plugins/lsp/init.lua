27c0a64fb5c12b57034171e98875102ef2c5ca73
Jason Cox <steel300@gmail.com>
2022-12-04 22:09:39 -0600

    chore(neovim): Flesh out the sumneko config more

--- modules/home-manager/nvim/lua/plugins/lsp/init.lua
+++ modules/home-manager/nvim/lua/plugins/lsp/init.lua
@@ -78,13 +78,52 @@ require("rust-tools").setup({
 require("neodev").setup({})
 
 lspconfig.sumneko_lua.setup(make_config({
+  single_file_support = true,
   settings = {
     Lua = {
       hint = {
         enable = true,
       },
+      workspace = {
+        checkThirdParty = false,
+      },
       completion = {
-        callSnippet = "Replace",
+        workspaceWord = true,
+        callSnippet = "Both",
+      },
+      misc = {
+        parameters = {
+          "--log-level=trace",
+        },
+      },
+      diagnostics = {
+        groupSeverity = {
+          strong = "Warning",
+          strict = "Warning",
+        },
+        groupFileStatus = {
+          ["ambiguity"] = "Opened",
+          ["await"] = "Opened",
+          ["codestyle"] = "None",
+          ["duplicate"] = "Opened",
+          ["global"] = "Opened",
+          ["luadoc"] = "Opened",
+          ["redefined"] = "Opened",
+          ["strict"] = "Opened",
+          ["strong"] = "Opened",
+          ["type-check"] = "Opened",
+          ["unbalanced"] = "Opened",
+          ["unused"] = "Opened",
+        },
+        unusedLocalExclude = { "_*" },
+      },
+      format = {
+        enable = false,
+        defaultConfig = {
+          indent_style = "space",
+          indent_size = "2",
+          continuation_indent_size = "2",
+        },
       },
     },
   },
