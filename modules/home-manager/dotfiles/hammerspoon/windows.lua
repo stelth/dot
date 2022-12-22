0e7c1791120ec22a078385996b8d60b43e225bfa
Jason Cox <steel300@gmail.com>
2022-12-21 21:56:52 -0600

    feat(hammerspoon): Use hammerspoon layouts to position windows

--- modules/home-manager/dotfiles/hammerspoon/windows.lua
+++ modules/home-manager/dotfiles/hammerspoon/windows.lua
@@ -5,64 +5,65 @@ hs.application.enableSpotlightForNameSearches(true)
 
 local spaces = require("hs.spaces")
 local wf = hs.window.filter
-local eventtap = hs.eventtap
-local timer = hs.timer
 
-local maxScreens = 6
 local sleep_interval = 1
-
-local logger = hs.logger.new("windowManager")
-logger.setLogLevel("debug")
 
 local sleep = function(n)
   os.execute("sleep " .. tonumber(n))
 end
 
-local positionApp = function(appTitle, screen, space)
-  logger.d("Positioning " .. appTitle)
+local positionApp = function(appTitle, screen, space, layout)
   if hs.application.get(appTitle) == nil then
-    logger.e("Application " .. appTitle .. " not found")
     return
   end
 
   hs.application.get(appTitle):activate()
   local windows = wf.new(appTitle):setCurrentSpace(nil):getWindows()
-  if #windows == 0 then
-    logger.w("No windows found for " .. appTitle)
-  end
-  for _, v in pairs(windows) do
-    logger.w("Positioning window " .. v:id() .. " of app " .. appTitle)
-    v:moveToScreen(screen)
-    logger.d(space)
-    spaces.moveWindowToSpace(v:id(), space)
-    v:maximize()
+  for _, win in pairs(windows) do
+    win:moveToScreen(screen)
+    spaces.moveWindowToSpace(win:id(), space)
+    spaces.gotoSpace(space)
+    hs.layout.apply({
+      { nil, win, screen, layout, nil, nil },
+    })
     sleep(sleep_interval)
+  end
+end
+
+local ensureSpaces = function(screen, numSpaces)
+  local allSpaces = hs.spaces.spacesForScreen(screen:id())
+
+  table.sort(allSpaces)
+
+  for _ = 1, numSpaces - #allSpaces, 1 do
+    hs.spaces.addSpaceToScreen(screen)
   end
 end
 
 M.officeMobile = function()
   -- Get screens
   local screen = hs.screen.mainScreen()
+  ensureSpaces(screen, 4)
+
   local allSpaces = hs.spaces.spacesForScreen(screen:id())
 
   table.sort(allSpaces)
 
-  positionApp("kitty", screen, allSpaces[1])
-  positionApp("Google Chrome", screen, allSpaces[1])
-  positionApp("Discord", screen, allSpaces[2])
-  positionApp("Firefox Developer Edition", screen, allSpaces[3])
-  positionApp("Microsoft Outlook", screen, allSpaces[4])
-  positionApp("Slack", screen, allSpaces[4])
+  positionApp("kitty", screen, allSpaces[1], hs.layout.left50)
+  positionApp("Google Chrome", screen, allSpaces[1], hs.layout.right50)
+  positionApp("Discord", screen, allSpaces[2], hs.layout.maximize)
+  positionApp("Firefox Developer Edition", screen, allSpaces[3], hs.layout.maximize)
+  positionApp("Microsoft Outlook", screen, allSpaces[4], hs.layout.left50)
+  positionApp("Slack", screen, allSpaces[4], hs.layout.right50)
 end
 
 M.office = function()
   -- Get screens
-  local leftScreen
-  local rightScreen
-  local centerScreen
+  local leftScreen = nil
+  local rightScreen = nil
+  local centerScreen = nil
   for _, v in pairs(hs.screen.allScreens()) do
     local x, _ = v:position()
-    logger.d(tostring(x))
 
     if x == -1 then
       leftScreen = v
@@ -73,6 +74,15 @@ M.office = function()
     end
   end
 
+  if leftScreen == nil or rightScreen == nil then
+    hs.alert.show("Did not detect 3 monitors")
+    return
+  end
+
+  ensureSpaces(leftScreen, 1)
+  ensureSpaces(centerScreen, 1)
+  ensureSpaces(rightScreen, 1)
+
   local leftSpaces = hs.spaces.spacesForScreen(leftScreen:id())
   table.sort(leftSpaces)
   local centerSpaces = hs.spaces.spacesForScreen(centerScreen:id())
@@ -80,17 +90,17 @@ M.office = function()
   local rightSpaces = hs.spaces.spacesForScreen(rightScreen:id())
   table.sort(rightSpaces)
 
-  positionApp("kitty", leftScreen, leftSpaces[1])
-  positionApp("Firefox Developer Edition", leftScreen, leftSpaces[1])
+  positionApp("kitty", leftScreen, leftSpaces[1], hs.layout.left50)
+  positionApp("Firefox Developer Edition", leftScreen, leftSpaces[1], hs.layout.right50)
 
-  positionApp("Google Chrome", centerScreen, centerSpaces[1])
-  positionApp("Discord", centerScreen, centerSpaces[1])
+  positionApp("Google Chrome", centerScreen, centerSpaces[1], hs.layout.left50)
+  positionApp("Discord", centerScreen, centerSpaces[1], hs.layout.right50)
 
-  positionApp("Microsoft Outlook", rightScreen, rightSpaces[1])
-  positionApp("Slack", rightScreen, rightSpaces[1])
+  positionApp("Microsoft Outlook", rightScreen, rightSpaces[1], hs.layout.left50)
+  positionApp("Slack", rightScreen, rightSpaces[1], hs.layout.right50)
 end
 
-menubar = hs.menubar.new()
+local menubar = hs.menubar.new()
 menubar:setIcon(hs.image.imageFromName("NSHandCursor"))
 
 if menubar then
