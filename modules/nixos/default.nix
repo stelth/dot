89e0d9c406f62361cde7d89769b8311736f861b4
Jason Cox <steel300@gmail.com>
2023-03-31 11:46:47 -0500

    chore(nix): Try systemd-boot

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -32,8 +32,9 @@
   networking.hostName = "nixDev"; # Define your hostname.
   networking.networkmanager.enable = true;
 
+  boot.loader.systemd-boot.enable = true;
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
+  boot.loader.grub.enable = false;
   boot.loader.grub.version = 2;
   boot.loader.grub.efiSupport = true;
   # Define on which hard drive you want to install Grub.
