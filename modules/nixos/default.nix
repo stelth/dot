a37a9b2af832acdd8024afb0c6be66e40a5241a8
Jason Cox <steel300@gmail.com>
2023-03-31 09:59:03 -0500

    chore(nix): Try readOnlyStore here as well

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -38,6 +38,8 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
+  boot.readOnlyStore = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
