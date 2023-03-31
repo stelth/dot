af545ff8eea6da8d94614371f2f0e6010574a867
Jason Cox <steel300@gmail.com>
2023-03-31 12:13:00 -0500

    chore(nix): Try new grub config

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -36,9 +36,9 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.devices = ["nodev"]; # or "nodev" for efi only
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
