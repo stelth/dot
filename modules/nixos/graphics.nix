ccef5cbb8966f77a12c6d996d1a506bdec7d6853
Jason Cox <steel300@gmail.com>
2023-04-03 13:55:45 -0500

    chore(nixos): Install and start polkit_gnome so 1password auth works

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -25,13 +25,34 @@
     text = "auth include login";
   };
 
+  environment.systemPackages = with pkgs; [polkit_gnome];
+
   hm = {
     wayland.windowManager.sway = {
       enable = true;
+      wrapperFeatures = {
+        gtk = true;
+      };
 
       config = rec {
         modifier = "Mod4";
         terminal = "kitty";
+      };
+    };
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
       };
     };
   };
