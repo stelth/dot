fb7832fc05318c0268a6162e1984124675f3c8c6
Jason Cox <steel300@gmail.com>
2023-04-04 15:14:11 -0500

    feat(nixos): Fill out keybindings and more for sway

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -47,8 +47,94 @@
         menu = "${pkgs.wofi}/bin/wofi --show run";
         startup = [
           {command = "1password";}
+          {command = "google-chrome-stable";}
+          {command = "kitty";}
+          {command = "discord";}
         ];
+        keybindings = let
+          inherit modifier;
+        in
+          pkgs.lib.mkOptionDefault {
+            # Focus
+            "${modifier}+Left" = "focus left";
+            "${modifier}+Right" = "focus right";
+            "${modifier}+Up" = "focus up";
+            "${modifier}+Down" = "focus down";
+
+            # Move
+            "${modifier}+Shift+Left" = "focus left";
+            "${modifier}+Shift+Right" = "focus right";
+            "${modifier}+Shift+Up" = "focus up";
+            "${modifier}+Shift+Down" = "focus down";
+
+            "${modifier}+Shift+space" = "floating toggle";
+            "${modifier}+space" = "focus mode_toggle";
+            "${modifier}+h" = "split h";
+            "${modifier}+v" = "split v";
+            "${modifier}+f" = "fullscreen toggle";
+            "${modifier}+comma" = "layout stacking";
+            "${modifier}+period" = "layout tabbed";
+            "${modifier}+slash" = "layout toggle split";
+            "${modifier}+a" = "focus parent";
+            "${modifier}+s" = "focus child";
+
+            "${modifier}+Shift+c" = "reload";
+            "${modifier}+Shift+r" = "restart";
+
+            # modes
+            "${modifier}+Shift+v" = ''mode "system: [r]eboot [p]oweroff [l]ogout'';
+            "${modifier}+r" = "mode resize";
+
+            "${modifier}+minus" = "scratchpad show";
+            "${modifier}+underscore" = "move container to scratchpad";
+          };
         bars = [{command = "${config.programs.waybar.package}/bin/waybar";}];
+        modes = {
+          "system: [r]eboot [p]oweroff [l]ogout" = {
+            r = "exec reboot";
+            p = "exec poweroff";
+            l = "exit";
+            Return = "mode default";
+            Escape = "mode default";
+          };
+          resize = {
+            Left = "resize shrink width";
+            Right = "resize grow width";
+            Down = "resize shrink height";
+            Up = "resize grow height";
+            Return = "mode default";
+            Escape = "mode default";
+          };
+        };
+        window.commands = [
+          {
+            command = "inhibit_idle fullscreen";
+            criteria.class = "Google-chrome";
+          }
+          {
+            command = "move to scratchpad";
+            criteria = {
+              class = "1Password";
+            };
+          }
+        ];
+        assigns = {
+          "1" = [{app_id = "kitty";}];
+          "2" = [
+            {class = "Google-chrome";}
+            {class = "discord";}
+          ];
+        };
+        workspaceOutputAssign = [
+          {
+            workspace = "1";
+            output = "DP-1";
+          }
+          {
+            workspace = "2";
+            output = "DP-2";
+          }
+        ];
       };
     };
 
