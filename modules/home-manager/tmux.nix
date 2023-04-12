a276f755fe8ab28f6d2b9b0acea503360c6809a6
Jason Cox <steel300@gmail.com>
2023-04-12 08:45:33 -0500

    chore(tmux): Use home-manager options to configure key modes and mouse

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -4,6 +4,8 @@
     prefix = "C-a";
     baseIndex = 1;
     terminal = "tmux-256color";
+    keyMode = "vi";
+    mouse = true;
     disableConfirmationPrompt = true;
     clock24 = true;
     plugins = with pkgs; [
@@ -12,7 +14,6 @@
       tmuxPlugins.yank
     ];
     extraConfig = ''
-      set -g mouse on
       bind | split-window -h
       bind - split-window -v
 
