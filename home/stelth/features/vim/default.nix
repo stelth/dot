57516cf8822f03e812b87e164cf5dc6a8afa63c0
Jason Cox <steel300@gmail.com>
2023-05-16 14:31:56 -0500

    chore(vim): Switch to vim-wayland-clipboard on linux

--- home/stelth/features/vim/default.nix
+++ home/stelth/features/vim/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
@@ -18,29 +19,30 @@
     extraConfig = import ./config.nix {
       inherit config pkgs;
     };
-    plugins = with pkgs.vimPlugins; [
-      auto-pairs
-      fzf-vim
-      is-vim
-      markdown-preview-nvim
-      undotree
-      vim-dispatch
-      vim-eunuch
-      vim-flagship
-      vim-fugitive
-      vim-manpager
-      vim-orgmode
-      vim-pager
-      vim-repeat
-      vim-sensible
-      vim-signify
-      vim-sleuth
-      vim-speeddating
-      vim-surround
-      vim-system-copy
-      vim-tmux-navigator
-      vim-unimpaired
-      vim-vinegar
-    ];
+    plugins = with pkgs.vimPlugins;
+      [
+        auto-pairs
+        fzf-vim
+        is-vim
+        markdown-preview-nvim
+        undotree
+        vim-dispatch
+        vim-eunuch
+        vim-flagship
+        vim-fugitive
+        vim-manpager
+        vim-orgmode
+        vim-pager
+        vim-repeat
+        vim-sensible
+        vim-signify
+        vim-sleuth
+        vim-speeddating
+        vim-surround
+        vim-tmux-navigator
+        vim-unimpaired
+        vim-vinegar
+      ]
+      ++ lib.optionals pkgs.stdenvNoCC.isLinux [vim-wayland-clipboard];
   };
 }
--- pkgs/vim-plugins/default.nix
+++ pkgs/vim-plugins/default.nix
@@ -1,5 +1,3 @@
 {pkgs}: {
-  lsp = pkgs.callPackage ./lsp {};
   vim-cmake = pkgs.callPackage ./vim-cmake {};
-  vim-system-copy = pkgs.callPackage ./vim-system-copy {};
 }
