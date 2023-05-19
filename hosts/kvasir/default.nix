b6a694ebb8078cb1c15d38c5edadfd5d9c38e25d
Jason Cox <steel300@gmail.com>
2023-05-19 10:03:43 -0500

    feat(nixos): Add optional udev rules to program the keyboard

--- hosts/kvasir/default.nix
+++ hosts/kvasir/default.nix
@@ -13,6 +13,7 @@
     ../common/users/stelth
 
     ../common/optional/1password.nix
+    ../common/optional/ergodox.nix
     ../common/optional/libvirt.nix
     ../common/optional/pipewire.nix
     ../common/optional/podman.nix
