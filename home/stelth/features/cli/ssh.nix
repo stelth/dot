70848e6b27fab531500d0d029cb793e69dd0d9e3
Jason Cox <steel300@gmail.com>
2023-05-23 15:58:07 -0500

    feat(ssh): Add ssh config for build node jump host

--- home/stelth/features/cli/ssh.nix
+++ home/stelth/features/cli/ssh.nix
@@ -36,6 +36,15 @@ in {
         serverAliveInterval = 50;
         extraOptions = {"StrictHostKeyChecking" = "no";};
       };
+      "10.140.100.*" = lib.hm.dag.entryBefore ["10.137.24.4 10.137.23.4 10.137.25.4 10.137.26.4 172.19.22.112"] {
+        user = "jcox";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519";
+        serverAliveInterval = 50;
+        extraOptions = {
+          "StrictHostKeyChecking" = "no";
+        };
+      };
       "github.ibm.com" = lib.hm.dag.entryBefore ["github.com"] {
         user = "Jason.P.Cox@ibm.com";
       };
