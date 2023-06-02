836a4b6c4becde3cb44a12b6cce4f3270f26f23a
Jason Cox <steel300@gmail.com>
2023-06-02 10:22:22 -0500

    chore(nix): Move configuration into their own file for modularization

---
+++ hosts/kvasir/configuration.nix
@@ -0,0 +1,30 @@
+{pkgs, ...}: {
+  networking = {
+    hostName = "kvasir";
+  };
+
+  programs = {
+    dconf.enable = true;
+    kdeconnect.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
+
+  security.polkit.enable = true;
+
+  system.stateVersion = "22.05";
+}
--- hosts/kvasir/default.nix
+++ hosts/kvasir/default.nix
@@ -1,12 +1,9 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
     inputs.hardware.nixosModules.common-pc-ssd
 
+    ./configuration.nix
     ./hardware-configuration.nix
 
     ../common/global
@@ -20,33 +17,4 @@
     ../common/optional/sddm.nix
     ../common/optional/workvpn.nix
   ];
-
-  networking = {
-    hostName = "kvasir";
-  };
-
-  programs = {
-    dconf.enable = true;
-    kdeconnect.enable = true;
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal-gtk
-    ];
-  };
-
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-  };
-
-  security.polkit.enable = true;
-
-  system.stateVersion = "22.05";
 }
