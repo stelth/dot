dd67343fd0797648d51a471c4f3de136e7abee6c
Jason Cox <steel300@gmail.com>
2023-06-22 12:07:27 -0500

    fix(nixos): Fix disappearing cursor under nvidia hardware

--- home/stelth/features/desktop/common/default.nix
+++ home/stelth/features/desktop/common/default.nix
@@ -13,7 +13,7 @@
 
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSOR = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
