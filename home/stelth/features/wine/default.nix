730ace1376f3637e3bbb52396accad0fc84201c8
Jason Cox <steel300@gmail.com>
2023-06-23 08:24:38 -0500

    chore(wine): Make wine/bottles work better

--- home/stelth/features/wine/default.nix
+++ home/stelth/features/wine/default.nix
@@ -1,15 +1,38 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     bottles
+    gamescope
+    lutris
     wineWowPackages.waylandFull
     winetricks
+
+    # Steam
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXinerama
+    xorg.libXScrnSaver
+    libpng
+    libvorbis
+    stdenv.cc.cc.lib
+    libkrb5
+    keyutils
+    protontricks
   ];
 
   home.persistence = {
     "/persist/home/stelth" = {
       allowOther = true;
       directories = [
-        ".local/share/bottles"
+        {
+          directory = ".local/share/bottles";
+          method = "symlink";
+        }
+        {
+          directory = "Games/Lutris";
+          method = "symlink";
+        }
+        ".config/lutris"
+        ".local/share/lutris"
       ];
     };
   };
