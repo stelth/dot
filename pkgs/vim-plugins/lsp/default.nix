503dba648076ea4cd69cfe6326445f4ec67ba543
Jason Cox <steel300@gmail.com>
2023-07-28 15:04:24 -0500

    chore(vim): Update lsp

--- pkgs/vim-plugins/lsp/default.nix
+++ pkgs/vim-plugins/lsp/default.nix
@@ -6,12 +6,12 @@
 in
   vimUtils.buildVimPlugin {
     inherit pname;
-    version = "2023-07-09";
+    version = "2023-07-28";
     src = fetchFromGitHub {
       owner = "yegappan";
       repo = "lsp";
-      rev = "5c3494c71577efc757d86064c8fa53d5f3d6c804";
-      sha256 = "w7/XBPD2FEPg212hszcKZ033DYDWzG4yq13LrS5xdzY=";
+      rev = "e117912131d18ece0c97248e142606da2844c56d";
+      sha256 = "37sPeWybCSE2jF4ObxfuBOPPchUtlCjEH4QMAmsbOlk=";
     };
     meta.homepage = "https://github.com/yegappan/lsp";
   }
