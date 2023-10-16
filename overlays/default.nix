85fd5350cd6e9d7d9baa70520877f3a5cd7095a2
Jason Cox <steel300@gmail.com>
2023-10-16 14:45:06 -0500

    chore(nixos): We don't need the nur overlay, just the home-manager module

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,17 +7,8 @@
     config,
     pkgs,
     final,
-    system,
     ...
   }: {
-    _module.args.pkgs = import inputs.nixpkgs {
-      inherit system;
-
-      overlays = [
-        inputs.nur.overlay
-      ];
-    };
-
     overlayAttrs = {
       inherit
         (config.packages)
