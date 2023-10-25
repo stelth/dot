8c289d1f5150e6b455be53d7239659b21da73787
Jason Cox <steel300@gmail.com>
2023-10-25 16:03:05 -0500

    feat(nixos): Add devShells and homeConfigurations to the checks

--- flake.nix
+++ flake.nix
@@ -94,8 +94,10 @@
         checks = let
           nixosMachines = lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) ((lib.filterAttrs (_: config: config.pkgs.system == system)) self.nixosConfigurations);
           packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+          devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
+          homeConfigurations = lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) (self'.legacyPackages.homeConfigurations or {});
         in
-          nixosMachines // packages;
+          nixosMachines // packages // devShells // homeConfigurations;
       };
 
       flake = {
