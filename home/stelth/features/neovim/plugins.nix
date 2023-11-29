5285cf6bbd55c439c9b63089a6d9441dbb83de7f
Jason Cox <steel300@gmail.com>
2023-11-29 17:50:51 -0600

    chore(neovim): Move required neovim packages into extraPackages instead of home.packages

--- home/stelth/features/neovim/plugins.nix
+++ home/stelth/features/neovim/plugins.nix
@@ -1,59 +1,59 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    # C/C++
-    clang-tools
-    cppcheck
-    cpplint
-    include-what-you-use
-    lldb
-
-    # Java
-    jdt-language-server
-
-    #nix
-    alejandra
-    deadnix
-    rnix-lsp
-    statix
-
-    # python
-    (python3.withPackages
-      (ps: with ps; [black debugpy flake8 isort yamllint pylint]))
-    nodePackages.pyright
-
-    # Lua
-    stylua
-    lua-language-server
-
-    # Docker
-    nodePackages.dockerfile-language-server-nodejs
-    hadolint
-
-    # Shell scripting
-    nodePackages.bash-language-server
-    shellcheck
-    shellharden
-    shfmt
-
-    # {C}Make
-    checkmake
-    cmake-format
-    cmake-language-server
-
-    # Additional
-    actionlint
-    gitlint
-    marksman
-    nodePackages.jsonlint
-    nodePackages.markdownlint-cli
-    nodePackages.prettier
-    nodePackages.vscode-json-languageserver
-    nodePackages.yaml-language-server
-    proselint
-    taplo
-  ];
-
   programs.neovim = {
+    extraPackages = with pkgs; [
+      # C/C++
+      clang-tools
+      cppcheck
+      cpplint
+      include-what-you-use
+      lldb
+
+      # Java
+      jdt-language-server
+
+      #nix
+      alejandra
+      deadnix
+      rnix-lsp
+      statix
+
+      # python
+      (python3.withPackages
+        (ps: with ps; [black debugpy flake8 isort yamllint pylint]))
+      nodePackages.pyright
+
+      # Lua
+      stylua
+      lua-language-server
+
+      # Docker
+      nodePackages.dockerfile-language-server-nodejs
+      hadolint
+
+      # Shell scripting
+      nodePackages.bash-language-server
+      shellcheck
+      shellharden
+      shfmt
+
+      # {C}Make
+      checkmake
+      cmake-format
+      cmake-language-server
+
+      # Additional
+      actionlint
+      gitlint
+      marksman
+      nodePackages.jsonlint
+      nodePackages.markdownlint-cli
+      nodePackages.prettier
+      nodePackages.vscode-json-languageserver
+      nodePackages.yaml-language-server
+      proselint
+      taplo
+    ];
+
     plugins = with pkgs.vimPlugins; [
       barbecue-nvim
       cmp-buffer
