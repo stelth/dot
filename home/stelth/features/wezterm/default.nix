643ab9d783d73888e3c00244b2cda9cff2947f33
Jason Cox <steel300@gmail.com>
2023-12-14 10:00:29 -0600

    Revert "chore(neovim): Factor out wezterm overrides so the program definition is easier to read"
    This reverts commit f87358b209f9833fe0f188eaf4ded59181111597.

--- home/stelth/features/wezterm/default.nix
+++ home/stelth/features/wezterm/default.nix
@@ -1,35 +1,33 @@
-{pkgs, ...}: let
-  wezterm = pkgs.wezterm.overrideAttrs (_: rec {
-    name = "wezterm-${version}";
-    version = "2023-12-03";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "wez";
-      repo = "wezterm";
-      rev = "e3cd2e93d0ee5f3af7f3fe0af86ffad0cf8c7ea8";
-      fetchSubmodules = true;
-      hash = "sha256-sj3S1fWC6j9Q/Yc+4IpLbKC3lttUWFk65ROyCdQt+Zc=";
-    };
-
-    cargoDeps = pkgs.rustPlatform.importCargoLock {
-      lockFile = ./Cargo.lock;
-      outputHashes = {
-        "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
-        "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
-      };
-    };
-
-    doCheck = false;
-
-    postPatch = ''
-      rm Cargo.lock
-      ln -s ${./Cargo.lock} Cargo.lock
-    '';
-  });
-in {
+{pkgs, ...}: {
   programs.wezterm = {
     enable = true;
-    package = wezterm;
+    package = pkgs.wezterm.overrideAttrs (_: rec {
+      name = "wezterm-${version}";
+      version = "2023-12-03";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "wez";
+        repo = "wezterm";
+        rev = "e3cd2e93d0ee5f3af7f3fe0af86ffad0cf8c7ea8";
+        fetchSubmodules = true;
+        hash = "sha256-sj3S1fWC6j9Q/Yc+4IpLbKC3lttUWFk65ROyCdQt+Zc=";
+      };
+
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = ./Cargo.lock;
+        outputHashes = {
+          "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
+          "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
+        };
+      };
+
+      doCheck = false;
+
+      postPatch = ''
+        rm Cargo.lock
+        ln -s ${./Cargo.lock} Cargo.lock
+      '';
+    });
 
     extraConfig = ''
       return {
