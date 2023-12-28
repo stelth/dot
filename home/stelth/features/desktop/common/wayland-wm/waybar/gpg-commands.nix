8a8d2792ac7c6285baa77051b4ef924befb52269
Jason Cox <steel300@gmail.com>
2023-12-28 10:39:55 -0600

    chore(waybar): Update yubikey detected command

--- home/stelth/features/desktop/common/wayland-wm/waybar/gpg-commands.nix
+++ home/stelth/features/desktop/common/wayland-wm/waybar/gpg-commands.nix
@@ -2,6 +2,6 @@
   ssh = "${pkgs.openssh}/bin/ssh";
   gpg-connect-agent = "${pkgs.gnupg}/bin/gpg-connect-agent";
 in {
-  isUnlocked = "${pkgs.procps}/bin/pgrep 'gpg-agent' &> /dev/null && ${gpg-connect-agent} --hex 'learn' 'scd apdu 00 f1 00 00' /bye | ${pkgs.gnugrep}/bin/grep OK -q";
+  isUnlocked = "${pkgs.procps}/bin/pgrep 'gpg-agent' &> /dev/null && ${gpg-connect-agent} --hex 'scd apdu 00 f1 00 00' /bye | ${pkgs.gnugrep}/bin/grep OK -q";
   unlock = "${ssh} -T localhost -o StrictHostKeyChecking=no exit";
 }
