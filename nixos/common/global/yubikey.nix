e2836f2031c9606f257e05a407ec7e1dcfb9b006
Jason Cox <steel300@gmail.com>
2024-01-02 11:01:48 -0600

    fix(yubikey): Add udev rule to automatically detect gpg yubikey

--- nixos/common/global/yubikey.nix
+++ nixos/common/global/yubikey.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
   services = {
     pcscd.enable = true;
-    udev.packages = [pkgs.yubikey-personalization];
+    udev = {
+      packages = with pkgs; [yubikey-personalization gnupg];
+      extraRules = ''
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0407", RUN+="${pkgs.gnupg}/bin/gpg --card-status"
+      '';
+    };
   };
 
   security.pam.services = {
