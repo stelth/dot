9f56da1356f5b821be1e458bafbafac798e3bdba
Jason Cox <steel300@gmail.com>
2024-01-02 13:15:36 -0600

    chore(neovim): Add quick move up/down keymaps

--- home/stelth/features/neovim/settings/keymaps.nix
+++ home/stelth/features/neovim/settings/keymaps.nix
@@ -9,6 +9,9 @@
     return keymap_set(mode, lhs, rhs, opts)
   end
 
+  vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+  vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
   vim.keymap.set("n", "Y", "yg$")
   vim.keymap.set("n", "<C-d>", "<C-d>zz")
   vim.keymap.set("n", "<C-u>", "<C-u>zz")
