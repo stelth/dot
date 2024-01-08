04628bb4c2bb8c100166e3457c4a99ad050dcd58
Jason Cox <steel300@gmail.com>
2024-01-08 11:19:33 -0600

    fix(vim): Use Hoist to add coc statusline to flagship

--- home/stelth/features/vim/config.nix
+++ home/stelth/features/vim/config.nix
@@ -207,7 +207,7 @@
   " Add (Neo)Vim's native statusline support
   " NOTE: Please see `:h coc-status` for integrations with external plugins that
   " provide custom statusline: lightline.vim, vim-airline
-  set statusline^=%{coc#status()}%{get(b:,'coc_current_function','\')}
+  autocmd User Flags call Hoist("buffer", " %{coc#status()} ")
 
   " Mappings for CoCList
   " Show all diagnostics
