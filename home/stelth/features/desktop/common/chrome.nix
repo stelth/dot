a10f7aefefc52196c8657139aabf3387aca9024e
Jason Cox <steel300@gmail.com>
2024-01-19 08:56:16 -0600

    chore(chrome): Use home-manager to pass correct cli args to chrome instead of kde settings

--- home/stelth/features/desktop/common/chrome.nix
+++ home/stelth/features/desktop/common/chrome.nix
@@ -1,6 +1,7 @@
 {...}: {
   programs.google-chrome = {
     enable = true;
+    commandLineArgs = ["--enable-features=OzonePlatform" "--ozone-platform=wayland" "--use-angle=vulkan" "--use-cmd-decoder=passthrough"];
   };
 
   home = {
