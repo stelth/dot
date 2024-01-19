8e872cdeb4a8839688a27f43c44a3dd5c531489e
Jason Cox <steel300@gmail.com>
2024-01-19 09:10:37 -0600

    Revert "chore(chrome): Use home-manager to pass correct cli args to chrome instead of kde settings"
    This reverts commit a10f7aefefc52196c8657139aabf3387aca9024e.

--- home/stelth/features/desktop/common/chrome.nix
+++ home/stelth/features/desktop/common/chrome.nix
@@ -1,7 +1,6 @@
 {...}: {
   programs.google-chrome = {
     enable = true;
-    commandLineArgs = ["--enable-features=OzonePlatform" "--ozone-platform=wayland" "--use-angle=vulkan" "--use-cmd-decoder=passthrough"];
   };
 
   home = {
