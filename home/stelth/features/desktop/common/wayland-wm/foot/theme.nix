454f699e4539d881dfdf4749f94658fdf45f2803
Jason Cox <steel300@gmail.com>
2024-02-12 12:45:02 -0600

    chore(foot): Replace color scheme with official upstream catppuccin version

--- home/stelth/features/desktop/common/wayland-wm/foot/theme.nix
+++ home/stelth/features/desktop/common/wayland-wm/foot/theme.nix
@@ -1,21 +1,22 @@
 {...}: {
-  background = "191724";
-  foreground = "e0def4";
-  regular0 = "26233a"; # black (Overlay)
-  regular1 = "eb6f92"; # red (Love)
-  regular2 = "31748f"; # green (Pine)
-  regular3 = "f6c177"; # yellow (Gold)
-  regular4 = "9ccfd8"; # blue (Foam)
-  regular5 = "c4a7e7"; # magenta (Iris)
-  regular6 = "ebbcba"; # cyan (Rose)
-  regular7 = "e0def4"; # white (Text)
+  foreground = "cdd6f4"; # Text
+  background = "1e1e2e"; # Base
 
-  bright0 = "6e6a86"; # bright black (Overlay)
-  bright1 = "eb6f92"; # bright red (Love)
-  bright2 = "31748f"; # bright green (Pine)
-  bright3 = "f6c177"; # bright yellow (Gold)
-  bright4 = "9ccfd8"; # bright blue (Foam)
-  bright5 = "c4a7e7"; # bright magenta (Iris)
-  bright6 = "ebbcba"; # bright cyan (Rose)
-  bright7 = "e0def4"; # bright white (Text)
+  regular0 = "45475a"; # Surface 1
+  regular1 = "f38ba8"; # red
+  regular2 = "a6e3a1"; # green
+  regular3 = "f9e2af"; # yellow
+  regular4 = "89b4fa"; # blue
+  regular5 = "f5c2e7"; # pink
+  regular6 = "94e2d5"; # teal
+  regular7 = "bac2de"; # Subtext 1
+
+  bright0 = "585b70"; # Surface 2
+  bright1 = "f38ba8"; # red
+  bright2 = "a6e3a1"; # green
+  bright3 = "f9e2af"; # yellow
+  bright4 = "89b4fa"; # blue
+  bright5 = "f5c2e7"; # pink
+  bright6 = "94e2d5"; # teal
+  bright7 = "a6adc8"; # Subtext 0
 }
