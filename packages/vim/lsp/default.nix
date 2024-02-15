461bff3fae280a3d7e7f1b6d0bf6df5128f088b1
Jason Cox <steel300@gmail.com>
2024-02-15 08:48:31 -0600

    chore(vim): Update yegappan lsp

--- packages/vim/lsp/default.nix
+++ packages/vim/lsp/default.nix
@@ -3,7 +3,7 @@
   fetchFromGitHub,
 }: let
   pname = "vim9-lsp";
-  version = "2024-02-09";
+  version = "2024-02-14";
 in
   vimUtils.buildVimPlugin {
     inherit pname version;
@@ -11,8 +11,8 @@ in
     src = fetchFromGitHub {
       owner = "yegappan";
       repo = "lsp";
-      rev = "bc2f5c040e9073b6253fbed2aeee3d0d013e45f6";
-      sha256 = "sha256-su+yGoXkNHhMTnu9uh90f2zfiqtsv+FkckPxa/0Lm4w=";
+      rev = "87189faf0bd4a2f974408df2d8c261dbfc3348dd";
+      sha256 = "sha256-5oyaO5riYxFx+rKYzO4qUPuWfOtxI9oDOAjzKoteT34=";
     };
 
     meta.homepage = "https://www.github.com/yegappan/lsp";
